2a09772865f5167c437a25f7e47b6e181168e160
Fredrik Wrede <wrede.fredrik@gmail.com>
2017-11-28 16:50:09 +0100

    added header

--- mio/featureExtraction/featureExtractionBase.py
+++ mio/featureExtraction/featureExtractionBase.py
@@ -1,4 +1,22 @@
-from pandas import  DataFrame
+# Copyright 2017  Fredrik Wrede, Prashant Singh, and Andreas Hellander
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, 
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Feature Extraction Base Class
+
+"""
+# Imports
+from pandas import DataFrame
 import tsfresh
 import numpy as np
 
@@ -34,7 +52,7 @@ class FeatureExtractionBase():
 		except AssertionError:
 			print 'All datapoints in __.dataframe has already been computed or data is missing. Add new datapoints first.'
 		else:
-			non_computed.drop(['computed'], axis=1) #remove the 'computed' column  (required by tsfresh)
+			non_computed = non_computed.drop(['computed'], axis=1) #remove the 'computed' column  (required by tsfresh)
 
 			#compute the features using tsfresh
 			f_subset = tsfresh.extract_features(non_computed, column_id = "index", column_sort = "time", column_kind = None, column_value = None)
