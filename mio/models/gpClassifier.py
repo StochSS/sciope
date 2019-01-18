9c25eac4696a023b8ee24c7ac0b0f5489d9eed58
Fredrik Wrede <wrede.fredrik@gmail.com>
2019-01-18 14:04:31 +0100

    added GP classifier to models

---
+++ mio/models/gpClassifier.py
@@ -0,0 +1,47 @@
+# Copyright 2017 Prashant Singh, Andreas Hellander and Fredrik Wrede
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Gaussian Process Regression Surrogate Model using sklearn
+"""
+
+# Imports
+from modelBase import ModelBase
+from sklearn.gaussian_process import GaussianProcessClassifier
+
+
+# Class definition
+class GPCModel(ModelBase):
+	"""
+	We use the sklearn GP Regressor implementation here.
+	"""
+	def __init__(self):
+		self.name = 'GPCModel'
+		
+	
+	# train the GP model given the data
+	def train(self, inputs, targets):
+		# Scale the training data
+		self.scaleTrainingData(inputs, targets)
+		
+		# Train the model
+		self.model = GaussianProcessClassifier(n_restarts_optimizer=100)
+		self.model.fit(self.X, self.y)
+		
+	# Predict
+	# * NOTE * 
+	# GP returns the probabilty of belonging to a certian class
+	def predict(self, Xt):
+		# Predict
+		return self.model.predict_proba(Xt)
+		
