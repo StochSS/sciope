7e1fd5cdda00d2b54a4d0c4b7258f80e641fb332
Fredrik Wrede <wrede.fredrik@gmail.com>
2019-03-05 16:23:24 +0100

    Label propagation surrugate added

---
+++ mio/tests/test_features.py
@@ -0,0 +1,11 @@
+from mio.features.feature_extraction import generate_tsfresh_features
+from tsfresh.feature_extraction.settings import EfficientFCParameters
+import numpy as np
+import pytest
+
+
+def test_generate_tsfresh_features():
+    X = np.random.randn(2,100)
+    features = EfficientFCParameters()
+    test = generate_tsfresh_features(X, features)
+    
