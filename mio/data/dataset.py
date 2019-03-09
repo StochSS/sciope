5fb6099112181831e59ba213ce4908ea936d005a
Prashant Singh <prashant.singh@it.uu.se>
2019-03-09 15:52:26 +0100

    initial implementation of log scaling for summary stats

--- mio/data/dataset.py
+++ mio/data/dataset.py
@@ -18,6 +18,8 @@ Dataset Class
 # Imports
 import numpy as np
 from collections import OrderedDict
+from scipy.stats.mstats import mquantiles
+from scipy.stats import zscore
 
 
 # Class definition
@@ -26,21 +28,23 @@ class DataSet(object):
 	Class for defining a dataset for a modeling/optimization/inference run 
 	
 	Properties/variables:
-	* x					(inputs)
-	* y					(targets)
-	* ts				(time series)
-	* s 				(summary statistics)
+	* x							(inputs)
+	* y							(targets)
+	* ts						(time series)
+	* s 						(summary statistics)
+	* outlier_column_indices	(columns containing outliers)
 	* size
-	* configurations 	(OrderedDict with relavant information)
+	* configurations 			(OrderedDict with relavant information)
 	* ensembles? 
 
 	
 	Methods:
-	* impute 			(treat missing values in summary statistics data)
+	* impute 					(treat missing values in summary statistics data)
 	* scaler? 
-	* set_data			(set inputs and targets)
-	* get_size			(returns current size of the dataset)
-	* add_points		(updates the dataset to include new points)
+	* set_data					(set inputs and targets)
+	* get_size					(returns current size of the dataset)
+	* add_points				(updates the dataset to include new points)
+	* process_outliers			(check summary stats that contain outliers, and apply log scaling)
 
 	
 	"""
@@ -51,6 +55,7 @@ class DataSet(object):
 		self.y = None
 		self.ts = None
 		self.s = None
+		self.outlier_column_indices = None
 		self.configurations = OrderedDict()
 		self.size = 0
 		
@@ -63,6 +68,7 @@ class DataSet(object):
 		self.ts = time_series
 		self.s = summary_stats
 		self.size = self.x.shape[0]
+		self.process_outliers()
 	
 	def get_size(self):
 		"""
@@ -78,10 +84,38 @@ class DataSet(object):
 		self.x = np.concatenate((self.x, inputs))
 		self.y = np.concatenate((self.y, targets))
 		
-		if time_series.any() != None: 
+		if time_series.any() is not None:
 			self.ts = np.concatenate((self.ts, time_series))
 
-		if summary_stats.any() != None:
-			self.s = np.concatenate((self.s, ))
+		if summary_stats.any() is not None:
+			if self.outlier_column_indices is not None:
+				summary_stats[:, self.outlier_column_indices] = np.log(summary_stats[:, self.outlier_column_indices])
+			self.s = np.concatenate((self.s, summary_stats))
 		
 		self.size = self.x.shape[0]
+
+	def process_outliers(self, mode='iqr'):
+		"""
+		Check for outliers in calculated summary stats. Outliers are the few very high or very low values that can
+		potentially introduce bias in tasks such as parameter inference. One can either remove them, replace with mean
+		value, or use log scale for the statistic in question.
+		@ToDo: add removal and imputations as options in addition to iqr and z-score
+		:param mode: either use 'z-score' or inter-quantile range 'iqr'
+		:return: -
+		"""
+		if mode == 'zscore':
+			# This will give us per-feature/per-statistic z-scores
+			zscores = zscore(self.s, axis=0)
+
+			# Find columns where abs(zscore) > threshold
+			zscore_threshold = 3
+			self.outlier_column_indices = np.unique(np.argwhere(np.abs(zscores) > zscore_threshold)[:, 1])
+		else:
+			# Outlier detection using IQR
+			quants = mquantiles(self.s)
+			iqr = quants[2] - quants[0]
+			violations_left = x2 < quants[0] - 1.5 * iqr
+			violations_right = x2 > quants[2] + 1.5 * iqr
+			self.outlier_column_indices = np.unique(np.argwhere(violations_left | violations_right)[:, 1])
+
+		self.s[:, self.outlier_column_indices] = np.log(self.s[:, self.outlier_column_indices])
