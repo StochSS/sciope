fb8212aab248d85ddceed224073add1eefdeea61
Prashant Singh <prashant.singh@it.uu.se>
2019-03-19 13:26:56 +0100

    Dependency links should be automatically processed, added to requires

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1',
+    version='0.2',
 
     description='The mio toolbox for surrogate modeling, likelihood-free parameter inference and black-box optimization.',
     long_description=long_description,
@@ -76,7 +76,8 @@ setup(
         'numpy',
         'scipy',
         'scikit-learn',
-        'tsfresh'],
+        'tsfresh',
+        'GPflowOpt'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
