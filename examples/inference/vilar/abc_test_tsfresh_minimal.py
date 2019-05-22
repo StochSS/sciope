BugFix: Nan enabled distance scaling and incorrect MSE calculation for the Vilar model

examples/inference/vilar/abc_test_tsfresh_minimal.py
@@ -43,7 +43,8 @@ abc_instance = abc_inference.ABC(data, vilar.simulate, epsilon=0.1, prior_functi
 inf_res = abc_instance.infer(num_samples=200, batch_size=100)
 
 # Compare results
-true_params = [[50.0, 100.0, 50.0, 500.0, 0.01, 50.0, 50.0, 5.0, 1.0, 10.0, 0.5, 0.2, 1.0, 2.0, 1.0]]
+true_value = np.array(['50.0', '500.0', '0.01', '50.0', '50.0', '5.0', '10.0', '0.5', '1.0', '0.2', '1.0', '1.0', '2.0'
+                          , '50.0', '100.0'], dtype=float)
 print('Inferred parameters: ', abc_instance.results['inferred_parameters'])
 print('Inference error in MAE: ', mean_absolute_error(true_params, abc_instance.results['inferred_parameters']))
 print('Trial count:', abc_instance.results['trial_count'])

sciope/inference/abc_inference.py
@@ -106,7 +106,7 @@ class ABC(InferenceBase):
         global normalized_distances
         self.historical_distances.append(dist.ravel())
         all_distances = np.array(self.historical_distances)
-        divisor = np.asarray(all_distances.max(axis=0))
+        divisor = np.asarray(np.nanmax(all_distances, axis=0))
         normalized_distances = all_distances
         for j in range(0, len(divisor), 1):
             if divisor[j] > 0: