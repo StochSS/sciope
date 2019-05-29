68f672561d4a6f0bc34bfa2c64a9c5f40f395018
Prashant Singh <prashant.singh@it.uu.se>
2019-05-29 12:46:31 +0200

    multiple trajs examples

--- examples/inference/vilar/abc_test.py
+++ examples/inference/vilar/abc_test.py
@@ -30,7 +30,7 @@ data = np.loadtxt("datasets/vilar_dataset_specieA_50trajs_15time.dat", delimiter
 dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
 mm_prior = uniform_prior.UniformPrior(np.asarray(dmin), np.asarray(dmax))
-bs_stat = bs.Burstiness(mean_trajectories=False, use_logger=False)
+bs_stat = bs.Burstiness(mean_trajectories=True, use_logger=False)
 
 # Set up ABC
 abc_instance = abc_inference.ABC(data, vilar.simulate, epsilon=0.1, prior_function=mm_prior,
