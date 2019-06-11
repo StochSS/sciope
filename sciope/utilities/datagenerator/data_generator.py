5e28e22aa10208d1d708233705c49b4be9430bdc
Mattias <ma10s@hotmail.com>
2019-06-11 16:51:28 +0200

    updated datagenerator

--- sciope/utilities/datagenerator/data_generator.py
+++ sciope/utilities/datagenerator/data_generator.py
@@ -70,7 +70,7 @@ sim = vilar.simulate
 dg = DataGenerator(prior_function = prior_function, sim = sim)
 print("type dg: ", type(dg))
 [tp,sim_result] = dg.gen(batch_size=10)
-#print([s.compute() for s in sim_result])
+print("sim result shape: ",sim_result.shape)
 
 
 
