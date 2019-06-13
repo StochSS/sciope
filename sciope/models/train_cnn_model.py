f17e4e8bf77bcd32656e5818fc006e4d5c68ac40
Mattias <ma10s@hotmail.com>
2019-06-13 12:54:12 +0200

    changes

--- sciope/models/train_cnn_model.py
+++ sciope/models/train_cnn_model.py
@@ -4,4 +4,4 @@ import pickle
 # Get the data
 data_path = '/home/ubuntu/sciope/sciope/utilities/datagenerator/ds_vilar_ft100_ts501_tr1_speciesall/ds_vilar_ft100_ts501_tr1_speciesall0.p'
 
-dataset = pickle.load(data_path)
+dataset = pickle.load(open( data_path, "rb" ) )
