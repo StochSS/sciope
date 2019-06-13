1d08da9b44e1d5d7945eefdea1e5ad8be0246f18
Mattias <ma10s@hotmail.com>
2019-06-13 12:56:40 +0200

    changes

--- sciope/models/train_cnn_model.py
+++ sciope/models/train_cnn_model.py
@@ -5,3 +5,8 @@ import pickle
 data_path = '/home/ubuntu/sciope/sciope/utilities/datagenerator/ds_vilar_ft100_ts501_tr1_speciesall/ds_vilar_ft100_ts501_tr1_speciesall0.p'
 
 dataset = pickle.load(open( data_path, "rb" ) )
+
+thetas = dataset.x
+timeser = dataset.ts
+
+print("theta shape: ", thetas.shape, ", timeseries shape: ", timeser.shape)
