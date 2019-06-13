a8c26719ac61cf7bc87ad56ce2fd90ff1815d0e5
Mattias <ma10s@hotmail.com>
2019-06-13 13:09:47 +0200

    changes

--- sciope/models/train_cnn_model.py
+++ sciope/models/train_cnn_model.py
@@ -1,6 +1,6 @@
 
 import pickle
-from cnn_regressor import CNNModel
+from sciope.models.cnn_regressor import CNNModel
 
 # Get the data
 data_path = '/home/ubuntu/sciope/sciope/utilities/datagenerator/ds_vilar_ft100_ts501_tr1_speciesall/ds_vilar_ft100_ts501_tr1_speciesall0.p'
