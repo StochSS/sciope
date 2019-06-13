1d3453cb85f0434eff23bcba53649fbd40a2db98
Mattias <ma10s@hotmail.com>
2019-06-13 14:29:33 +0200

    changes

--- sciope/models/train_cnn_model.py
+++ sciope/models/train_cnn_model.py
@@ -1,13 +1,22 @@
 
 import pickle
+import os
 import numpy as np
 from sciope.models.cnn_regressor import CNNModel
 
 # Get the data
-data_path = '/home/ubuntu/sciope/sciope/utilities/datagenerator/ds_vilar_ft100_ts501_tr1_speciesall/ds_vilar_ft100_ts501_tr1_speciesall0.p'
-dataset = pickle.load(open( data_path, "rb" ) )
-theta = dataset.x
-ts = dataset.ts
+data_path = '/home/ubuntu/sciope/sciope/utilities/datagenerator/ds_vilar_ft100_ts501_tr1_speciesall' #/ds_vilar_ft100_ts501_tr1_speciesall0.p'
+
+
+
+for filename in os.listdir(data_path):
+    dataset = pickle.load(open(data_path, "rb" ) )
+    if theta is not None:
+        theta = np.append(theta, dataset.x, axis=0)
+        ts = np.append(ts, dataset.ts, axis=0)
+    else:
+        theta = dataset.x
+        ts = dataset.ts
 
 print("theta shape: ", theta.shape, ", timeseries shape: ", ts.shape)
 
