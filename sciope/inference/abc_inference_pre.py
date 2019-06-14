91075a2aec48599943d64b0974fe43ba5c2af9c7
Mattias <ma10s@hotmail.com>
2019-06-14 17:17:31 +0200

    changes

--- sciope/inference/abc_inference_pre.py
+++ sciope/inference/abc_inference_pre.py
@@ -283,7 +283,7 @@ class ABC():
         print("welcome to inf")
         print("time series shape: ", self.time_series.shape)
         data_s = [x for x in self.time_series]
-        print("data_s shape: ", np.array(data_s).shape)
+        print("data_s len: ", len(data_S))
 
         data_s = [self.summaries_function.compute(x) for x in self.time_series]
         max_s = np.max(data_s, axis=1)
