020638148202ca68cb75093ea31665e1ec25490f
Mattias <ma10s@hotmail.com>
2019-06-15 20:09:29 +0200

    changes

--- examples/inference/vilar/abc_test_pre.py
+++ examples/inference/vilar/abc_test_pre.py
@@ -107,7 +107,7 @@ fontsize = 12
 bins = np.linspace(0,1,11)
 for i in range(15):
     hist=axes[i].hist(abc_instance.results['accepted_samples'][:, i])
-    hist_max=np.max(hist)
+    hist_max=np.max(hist[0])
     axes[i].plot([dmin,dmin],[0,hist_max])
     axes[i].plot([dmin,dmin],[0,hist_max])
 
