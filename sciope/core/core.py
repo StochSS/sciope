4beceb65d5740e5784d72fc57072c706bc119003
Fredrik Wrede <wrede.fredrik@gmail.com>
2019-06-25 17:17:16 +0200

    parallel workflows moved to core, test for core

---
+++ sciope/core/core.py
@@ -0,0 +1,174 @@
+from dask.distributed import get_client
+from dask import delayed
+from toolz import partition_all
+import numpy as np
+
+
+def _cluster_mode():
+    try:
+        get_client()
+        return True
+    except ValueError:
+        return False
+
+
+def get_futures(lst):
+    """ Loop through items in list to keep order of delayed objects
+        when transforming to futures. firect call of futures_of does
+        not keep the order of the objects
+
+    Parameters
+    ----------
+    lst : array-like
+        array containing delayed objects
+    """
+    f = []
+    for i in lst:
+        f.append(futures_of(i)[0])
+    return f
+
+@delayed
+def delay_func_chunk(func, chunk):
+    res = []
+    for x in chunk:
+        res.append(func(x))
+    return res
+
+
+def get_graph_chunked(param_func, sim_func, summaries_func=None, dist_func=None,
+                   fixed=None, batch_size=10, chunk_size=2):
+    """
+    Constructs the dask computational graph involving sampling, simulation,
+    summary statistics and distances.
+
+    Parameters
+    ----------
+    param_func : callable
+        the parameter sampling function, see sciope.designs, sciope.sampling
+        and sciope.utilities.priors
+    sim_func : callable
+        the simulator function which takes a parameter point as argument
+    summaries_func : callable, optional
+        the summaries statistics function which takes a simulation result,
+        by default None
+    dist_func : callable, optional
+        the distance function between , by default None
+    batch_size : int, optional
+        the number of points being sampled in each batch, by default 10
+    chunk_size : int, optional
+        decription, by default 2
+    ensemble_size : int, optional
+        [description], by default 1
+
+        Returns
+    -------
+    dict
+        with keys 'parameters', 'trajectories', 'summarystats' and 'distances'
+        values being dask delayed objects
+    """
+    if dist_func is not None:
+        assert fixed is not None, "If using distance function, parameter 'fixed' can not be None"
+
+    # worflow sampling with batch size = batch_size
+
+    # Draw from the prior/design
+    trial_param = param_func(batch_size)
+
+    params_chunked = partition_all(chunk_size, trial_param)
+
+    # Perform the simulation
+
+    sim_result = [delay_func_chunk(sim_func, chunk) for chunk in params_chunked]
+
+    # Get the statistic(s)
+
+    if summaries_func is not None:
+        
+        stats_final = [delay_func_chunk(summaries_func, chunk) for chunk in sim_result]
+
+        # Calculate the distance between the dataset and the simulated result
+        
+    else:
+        stats_final = None
+        
+    return {"parameters": trial_param, "trajectories": sim_result, 
+            "summarystats": stats_final}
+
+def distance(dist_func, X, chunked=True):
+
+    if chuncked:
+        sim_dist = [delay_func_chunk(dist_func, chunk) for chunk in X_chunked]
+        
+    else:
+        sim_dist = [delayed(dist_func)(x) for x in X]
+    
+    return sim_dist 
+
+def get_graph_unchunked(param_func, sim_func, summaries_func=None, dist_func=None,
+                   fixed=None, batch_size=10, ensemble_size=1):
+    """
+    Constructs the dask computational graph involving sampling, simulation,
+    summary statistics and distances.
+
+    Parameters
+    ----------
+    param_func : callable
+        the parameter sampling function, see sciope.designs, sciope.sampling
+        and sciope.utilities.priors
+    sim_func : callable
+        the simulator function which takes a parameter point as argument
+    summaries_func : callable, optional
+        the summaries statistics function which takes a simulation result,
+        by default None
+    dist_func : callable, optional
+        the distance function between , by default None
+    batch_size : int, optional
+        the number of points being sampled in each batch, by default 10
+    chunk_size : int, optional
+        decription, by default 2
+    ensemble_size : int, optional
+        [description], by default 1
+
+        Returns
+    -------
+    dict
+        with keys 'parameters', 'trajectories', 'summarystats' and 'distances'
+        values being dask delayed objects
+    """
+    if dist_func is not None:
+        assert fixed is not None, "If using distance function, parameter 'fixed' can not be None"
+
+    # worflow sampling with batch size = batch_size
+
+    # Draw from the prior/design
+    trial_param = param_func(batch_size) * ensemble_size
+
+    # Perform the simulation
+
+    sim_result = [delayed(sim_func)(x) for x in trial_param]
+
+    # Get the statistic(s)
+
+    if summaries_func is not None:
+        
+        sim_stats = [delayed(summaries_func)(x) for x in sim_result]
+
+        if ensemble_size > 1:
+            stats_final = [delayed(np.mean)(sim_stats[i:i + ensemble_size], 
+                           axis=0) for i in range(0, len(sim_stats), ensemble_size)]
+        else:
+            stats_final = sim_stats
+
+        # Calculate the distance between the dataset and the simulated result
+        
+        if dist_func is not None:
+            sim_dist = [delayed(dist_func)(fixed, stats)
+                            for stats in stats_final]
+        else:
+            sim_dist = None
+    else:
+        stats_final = None
+        sim_dist = None
+
+    return {"parameters": trial_param[:batch_size], "trajectories": sim_result, 
+            "summarystats": stats_final, "distances": sim_dist}
---
+++ sciope/tests/test_core.py
@@ -0,0 +1,217 @@
+from dask.distributed import Client, get_client
+from sciope.utilities.priors import uniform_prior
+from sciope.designs.latin_hypercube_sampling import LatinHypercube
+from sciope.utilities.summarystats import burstiness as bs
+import numpy as np
+import sys
+from sciope.utilities.distancefunctions import naive_squared as ns
+from sciope.utilities.distancefunctions import euclidean as euc
+from sciope.core import core
+import pytest
+import gillespy2
+from gillespy2.solvers.numpy import NumPySSASolver
+import os
+import dask
+
+class ToggleSwitch(gillespy2.Model):
+    """ Gardner et al. Nature (1999)
+    'Construction of a genetic toggle switch in Escherichia coli'
+    """
+    def __init__(self, parameter_values=None):
+        # Initialize the model.
+        gillespy2.Model.__init__(self, name="toggle_switch")
+        # Parameters
+        alpha1 = gillespy2.Parameter(name='alpha1', expression=1)
+        alpha2 = gillespy2.Parameter(name='alpha2', expression=1)
+        beta = gillespy2.Parameter(name='beta', expression="2.0")
+        gamma = gillespy2.Parameter(name='gamma', expression="2.0")
+        mu = gillespy2.Parameter(name='mu', expression=1.0)
+        self.add_parameter([alpha1, alpha2, beta, gamma, mu])
+
+        # Species
+        U = gillespy2.Species(name='U', initial_value=10)
+        V = gillespy2.Species(name='V', initial_value=10)
+        self.add_species([U, V])
+
+        # Reactions
+        cu = gillespy2.Reaction(name="r1",reactants={}, products={U:1},
+                propensity_function="alpha1/(1+pow(V,beta))")
+        cv = gillespy2.Reaction(name="r2",reactants={}, products={V:1},
+                propensity_function="alpha2/(1+pow(U,gamma))")
+        du = gillespy2.Reaction(name="r3",reactants={U:1}, products={},
+                rate=mu)
+        dv = gillespy2.Reaction(name="r4",reactants={V:1}, products={},
+                rate=mu)
+        self.add_reaction([cu,cv,du,dv])
+        self.timespan(np.linspace(0,50,101))
+
+toggle_model = ToggleSwitch()
+
+# Define simulator function
+
+
+def set_model_parameters(params, model):
+    """ params - array, needs to have the same order as
+        model.listOfParameters """
+    for e, (pname, p) in enumerate(model.listOfParameters.items()):
+        model.get_parameter(pname).set_expression(params[e])
+    return model
+
+# Here we use gillespy2 numpy solver, so performance will
+# be quite slow for this model
+
+
+def simulator(params, model):
+
+    model_update = set_model_parameters(params, model)
+    num_trajectories = 1  # TODO: howto handle ensembles
+
+    res = model_update.run(solver=NumPySSASolver, show_labels=False,
+                           number_of_trajectories=num_trajectories)
+    tot_res = res[0][:, 1:]  # should not contain timepoints
+
+    return np.array(tot_res)
+
+
+def simulator2(x):
+    return simulator(x, model=toggle_model)
+
+# Set up the prior
+
+
+default_param = np.array(list(toggle_model.listOfParameters.items()))[:,1]
+bound = []
+for exp in default_param:
+    bound.append(float(exp.expression))
+    
+bound = np.array(bound)
+dmin = bound * 0.5
+dmax = bound * 2.0
+
+uni_prior = uniform_prior.UniformPrior(dmin, dmax)
+lhd = LatinHypercube(dmin, dmax)
+
+def simple_sampler(n):
+    res = []
+    for i in range(n):
+        res.append(dask.delayed(np.random.randn(2)))
+    return res
+
+def simple_sim(x):
+    return x + np.random.randn(2)
+
+
+def simple_summ(x):
+    return np.array([np.sum(x), np.min(x)])
+
+
+try:
+    c =get_client()
+except:
+    c = Client()
+
+
+def test_simple_unchunked():
+    
+    graph_dict = core.get_graph_unchunked(param_func=simple_sampler, sim_func=simple_sim, 
+                                    summaries_func=simple_summ, batch_size=10, ensemble_size=1)
+
+    assert len(graph_dict["parameters"]) == 10, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["trajectories"]) == 10, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["summarystats"]) == 10, "Core test failed, dimensions mismatch"
+
+    params, sim, summ = dask.compute(graph_dict["parameters"], graph_dict["trajectories"],graph_dict["summarystats"])
+
+    assert len(params) == 10, "Core test failed, dimensions mismatch"
+    assert len(sim) == 10, "Core test failed, dimensions mismatch"
+    assert len(summ) == 10, "Core test failed, dimensions mismatch"
+
+
+    graph_dict = core.get_graph_unchunked(param_func=simple_sampler, sim_func=simple_sim, 
+                                    summaries_func=simple_summ, batch_size=10, ensemble_size=2)
+
+    assert len(graph_dict["parameters"]) == 10, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["trajectories"]) == 20, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["summarystats"]) == 10, "Core test failed, dimensions mismatch"
+
+    params, sim, summ = dask.compute(graph_dict["parameters"], graph_dict["trajectories"],graph_dict["summarystats"])
+
+    assert len(params) == 10, "Core test failed, dimensions mismatch"
+    assert len(sim) == 20, "Core test failed, dimensions mismatch"
+    assert len(summ) == 10, "Core test failed, dimensions mismatch"
+
+
+    graph_dict = core.get_graph_unchunked(param_func=simple_sampler, sim_func=simple_sim, 
+                                    batch_size=10, ensemble_size=2)
+
+    assert len(graph_dict["parameters"]) == 10, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["trajectories"]) == 20, "Core test failed, dimensions mismatch"
+    assert graph_dict["summarystats"] is None, "Core test failed, excpected None"
+
+    params, sim = dask.compute(graph_dict["parameters"], graph_dict["trajectories"])
+
+    assert len(params) == 10, "Core test failed, dimensions mismatch"
+    assert len(sim) == 20, "Core test failed, dimensions mismatch"
+
+
+def test_simple_chunked():
+    
+    graph_dict = core.get_graph_chunked(param_func=simple_sampler, sim_func=simple_sim, 
+                                    summaries_func=simple_summ, batch_size=10, chunk_size=2)
+
+    assert len(graph_dict["parameters"]) == 10, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["trajectories"]) == 5, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["summarystats"]) == 5, "Core test failed, dimensions mismatch"
+
+    params, sim, summ = dask.compute(graph_dict["parameters"], graph_dict["trajectories"],graph_dict["summarystats"])
+
+    sim = np.asarray(sim)
+    summ = np.asarray(summ)
+
+    assert len(params) == 10, "Core test failed, dimensions mismatch"
+    assert sim.shape == (5, 2, 2), "Core test failed, dimensions mismatch"
+    assert summ.shape == (5, 2, 2), "Core test failed, dimensions mismatch"
+
+
+    graph_dict = core.get_graph_chunked(param_func=simple_sampler, sim_func=simple_sim, 
+                                    batch_size=10, chunk_size=2)
+
+    assert len(graph_dict["parameters"]) == 10, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["trajectories"]) == 5, "Core test failed, dimensions mismatch"
+    assert graph_dict["summarystats"] is None, "Core test failed, excpected None"
+
+    params, sim = dask.compute(graph_dict["parameters"], graph_dict["trajectories"])
+
+    sim = np.asarray(sim)
+
+    assert len(params) == 10, "Core test failed, dimensions mismatch"
+    assert sim.shape == (5, 2, 2), "Core test failed, dimensions mismatch"
+
+def test_param_sim():
+    n_points = 10
+    #graph_dict = core.get_dask_graph(
+    #    param_func=uni_prior.draw, sim_func=simulator2, batch_size=n_points)
+    #assert len(graph_dict["parameters"]
+    #           ) == 10, "Core test failed, dimensions mismatch"
+    #assert len(graph_dict["trajectories"]
+    #           ) == 10, "Core test failed, dimensions mismatch"
+    #assert graph_dict["summarystats"] is None, "Core test failed, expected None"
+    #assert graph_dict["distances"] is None, "Core test failed, expected None"
+
+    lhd.generate_array(n_points)
+    graph_dict = core.get_graph_chunked(
+        param_func=lhd.draw, sim_func=simulator2, batch_size=n_points, chunk_size=2)
+    assert len(graph_dict["parameters"]
+               ) == 10, "Core test failed, dimensions mismatch"
+    assert len(graph_dict["trajectories"]
+               ) == 5, "Core test failed, dimensions mismatch"
+    assert graph_dict["summarystats"] is None, "Core test failed, expected None"
+
+    params, sim = dask.compute(graph_dict["parameters"], graph_dict["trajectories"])
+
+    sim = np.asarray(sim)
+    params = np.asarray(params)
+
+    assert params.shape == (10,5), "Core test failed, dimensions mismatch"
+    assert sim.shape == (5, 2, 101, 2), "Core test failed, dimensions mismatch"
+    
