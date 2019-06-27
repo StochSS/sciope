375561d39e40e4f5e9d447f6a6540e1c5dec571e
Mattias <ma10s@hotmail.com>
2019-06-27 13:17:45 +0200

    changes

--- examples/inference/vilar/banditsabc_test.py
+++ examples/inference/vilar/banditsabc_test.py
@@ -60,7 +60,7 @@ ss= sum_stats.compute(data).compute()
 #ss = np.array([sum_stats.compute([data[:,i]]) for i in range(10)])
 print("ss shape: ", ss.shape)
 print("ss: ", ss)
-print("sum_stats features: ", sum_stats.features)
+print("sum_stats features: ", sum_stats.features.keys())
 
 
 abc_instance = bandits_abc.BanditsABC(data, vilar.simulate, epsilon=epsilon, prior_function=mm_prior, k=3,
