29b3f6e8f3e484870900870cebcb3ab0343891b9
Mattias <ma10s@hotmail.com>
2019-06-27 13:18:07 +0200

    changes

--- examples/inference/vilar/banditsabc_test.py
+++ examples/inference/vilar/banditsabc_test.py
@@ -59,7 +59,7 @@ print("data shape: ", data.shape)
 ss= sum_stats.compute(data).compute()
 #ss = np.array([sum_stats.compute([data[:,i]]) for i in range(10)])
 print("ss shape: ", ss.shape)
-print("ss: ", ss)
+#print("ss: ", ss)
 print("sum_stats features: ", sum_stats.features.keys())
 
 
