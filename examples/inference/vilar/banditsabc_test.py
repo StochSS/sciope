2cae1d480c4ab04611c5b98f174d4e79fecba861
Mattias <ma10s@hotmail.com>
2019-06-29 13:35:38 +0200

    changes

--- examples/inference/vilar/banditsabc_test.py
+++ examples/inference/vilar/banditsabc_test.py
@@ -120,7 +120,7 @@ abc_instance = bandits_abc.BanditsABC(data, vilar.simulate, epsilon=epsilon, pri
                                       mab_variant=mab_algo)
 
 # Perform ABC; require 30 samples
-abc_instance.infer(num_samples=200, batch_size=5)
+abc_instance.infer(num_samples=20, batch_size=5)
 
 # Results
 print('Inferred parameters: ', abc_instance.results['inferred_parameters'])
