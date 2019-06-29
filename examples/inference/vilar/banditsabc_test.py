8f90f2fb1a93b6c06433d8934cc79f421ed06e64
Mattias <ma10s@hotmail.com>
2019-06-29 13:49:31 +0200

    changes

--- examples/inference/vilar/banditsabc_test.py
+++ examples/inference/vilar/banditsabc_test.py
@@ -91,6 +91,24 @@ print("idxxx shape: ", idxxx.shape)
 sum_stats.set_returning_features(idxxx)
 
 
+
+
+trial_param = [mm_prior.draw() for i in range(5)]
+
+trial_param = dask.compute(trial_param)
+trial_sim = [vilar.simulate(np.array(t)) for t in trial_param]
+trial_ss = [sum_stats.compute(s) for s in trial_sim]
+trial_dist = [dist_fun.compute(ss,s) for s in trial_ss]
+trial_dist = dask.compute(trial_dist)
+print("trial_dist shape: ", np.array(trial_dist).shape)
+print("max dist: ", np.max(np.array(trial_dist),axis=0))
+max_dist=np.max(np.array(trial_dist),axis=0).squeeze()
+print("max dist shape", max_dist.shape)
+print("sorted dist: ", np.sort(max_dist))
+
+
+
+
 # Select MAB variant
 mab_algo = md.MABDirect(bandits_abc.arm_pull)
 
