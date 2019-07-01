1cffa09315ba4d1d8d9a30c35c54ff52dfd2edbf
Mattias <ma10s@hotmail.com>
2019-07-01 12:31:41 +0200

    changes

--- examples/inference/vilar/banditsabc_test.py
+++ examples/inference/vilar/banditsabc_test.py
@@ -105,7 +105,8 @@ sum_stats.set_returning_features(idxxx2)
 
 ss=sum_stats.compute(data).compute()
 print("trial_param shape: ", np.array(trial_param).shape)
-tp = np.array(trial_param).squeeze()
+tp = np.array(dask.compute(trial_param)).squeeze()
+print("tp shape: ", tp.shape)
 print("tp shape: ", tp.shape)
 
 plt.scatter(tp[:,0],tp[:,1])
