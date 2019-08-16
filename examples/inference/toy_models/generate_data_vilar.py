4a6e1ca69e70913cad122f8ee7464df3e64e2eeb
Mattias <ma10s@hotmail.com>
2019-08-16 13:34:14 +0200

    add the vilar data generator

--- examples/inference/toy_models/generate_data_vilar.py
+++ examples/inference/toy_models/generate_data_vilar.py
@@ -9,6 +9,7 @@ from sciope.utilities.summarystats import burstiness as bs
 import numpy as np
 import vilar
 from vilar import simulate
+import dask
 
 from sklearn.metrics import mean_absolute_error
 
@@ -17,20 +18,20 @@ modelname = "vilar_A_100_201"
 # Set up the prior
 dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
-prior = uniform_prior.UniformPrior(np.asarray(dmin), np.asarray(dmax))
+prior = uniform_prior.UniformPrior(np.asarray(dmin), np.asarray(dmax)).draw
 print("generating trainig data")
 n=100000
-train_thetas = prior(n)
+train_thetas = np.squeeze( np.array(dask.compute( prior(n))))
 train_ts =np.array([simulate(p) for p in train_thetas])
 print("generating trainig data done, shape: train_ts: ", train_ts.shape, ", train_thetas: ", train_thetas.shape)
 
-validation_thetas = np.array(prior(n=10000))
+validation_thetas = np.squeeze( np.array(dask.compute(np.array(prior(n=10000)))))
 validation_ts = np.expand_dims(np.array([simulate(p) for p in validation_thetas]),2)
 
-test_thetas = np.array(prior(n=10000))
+test_thetas = np.squeeze( np.array(dask.compute(np.array(prior(n=10000)))))
 test_ts = np.expand_dims(np.array([simulate(p) for p in validation_thetas]),2)
 
-abc_trial_thetas = np.array(prior(n=500000))
+abc_trial_thetas = np.squeeze( np.array(dask.compute( np.array(prior(n=500000)))))
 abc_trial_ts = np.expand_dims(np.array([simulate(p) for p in abc_trial_thetas]),2)
 
 pickle.dump( true_param, open( 'datasets/' + modelname + '/true_param.p', "wb" ) )
