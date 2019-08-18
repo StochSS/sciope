730b94bc97221c49eb866d13b20a9ff6d89c279c
Mattias <ma10s@hotmail.com>
2019-08-18 21:54:45 +0200

    add the vilar data generator

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -24,8 +24,8 @@ print("update")
 modelname = "vilar_ACR_100_201"
 dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
-train_thetas = pickle.load(open('datasets/' + modelname + '/train_thetas.p', "rb" ) )
-train_ts = pickle.load(open('datasets/' + modelname + '/train_ts.p', "rb" ) )
+# train_thetas = pickle.load(open('datasets/' + modelname + '/train_thetas.p', "rb" ) )
+# train_ts = pickle.load(open('datasets/' + modelname + '/train_ts.p', "rb" ) )
 
 train_thetas, train_ts = load_spec(modelname="vilar_ACR_100_201", type = "train")
 
