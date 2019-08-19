22bb499f0cce59fd5af477600e2add63e3ce6442
Mattias <ma10s@hotmail.com>
2019-08-19 11:42:07 +0200

    add the vilar data generator

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -52,10 +52,10 @@ nnm = PEN_CNNModel(input_shape=(201,3), output_shape=(15), pen_nr=10)
 # nm = ANNModel(input_shape=(100,1), output_shape=(2))
 
 
-# nnm.load_model()
+nnm.load_model()
 
-nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
-          plot_training_progress=False)
+# nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
+#           plot_training_progress=False)
 #
 # nnm.load_model()
 #validation_pred = np.array([nnm.predict(validation_ts[i*100:(i+1)*100]) for i in range(500)])
