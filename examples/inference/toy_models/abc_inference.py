2d30928b205c83c6512cd5df722c6e9880362b91
Mattias <ma10s@hotmail.com>
2019-08-19 16:37:07 +0200

    add the vilar data generator

--- examples/inference/toy_models/abc_inference.py
+++ examples/inference/toy_models/abc_inference.py
@@ -32,6 +32,7 @@ dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
 
 true_param = pickle.load(open('datasets/' + modelname + '/true_param.p', "rb" ) )
+true_param = np.array(true_param)
 print("true_param shape: ", true_param.shape)
 data = pickle.load(open('datasets/' + modelname + '/obs_data.p', "rb" ) )
 print("data shape: ", data.shape)
