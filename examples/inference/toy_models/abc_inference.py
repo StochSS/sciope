d5b8fa374a0e5c17fa4ff693c795069711aebd99
Mattias <ma10s@hotmail.com>
2019-08-19 22:33:44 +0200

    add the vilar data generator

--- examples/inference/toy_models/abc_inference.py
+++ examples/inference/toy_models/abc_inference.py
@@ -83,8 +83,13 @@ f.suptitle('Accepted/Trial = ' + str(nr_of_accept) + '/' + str(nr_of_trial),font
 for i in range(16):
     x = i // 4
     y = i % 4
+    if x == y:
+        ax[x, y].hist(accepted_para[:, bpi[x]])
+        ax[x, y].plot([true_param[bpi[x]], true_param[bpi[x]]], [0,1], c='black')
+        ax[x, y].plot([0, 1, 1, 0, 0], [0, 0, 1, 1, 0])
+
     ax[x,y].scatter(accepted_para[:, bpi[x]], accepted_para[:, bpi[y]], color="green", s=2)
-    ax[x,y].scatter(true_param[bpi[x]],true_param[bpi[y]], color="gray", marker="x")
+    ax[x,y].scatter(true_param[bpi[x]],true_param[bpi[y]], color="black", marker="*")
     ax[x,y].scatter(accepted_mean[bpi[x]],accepted_mean[bpi[y]], color="red", marker="x")
     ax[x,y].scatter(data_pred[bpi[x]],data_pred[bpi[y]], color="gray", marker="o")
     ax[x, y].plot([0,1,1,0,0],[0,0,1,1,0])
