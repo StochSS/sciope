c81506fe82750b9e6f6ab1f4a677ebd5ae5372c8
Mattias <ma10s@hotmail.com>
2019-08-21 15:47:02 +0200

    adjusting the subplots

--- examples/inference/toy_models/abc_inference2.py
+++ examples/inference/toy_models/abc_inference2.py
@@ -144,13 +144,14 @@ for x in range(15):
             ax[x, y].scatter(data_pred[y],data_pred[x], color="gray", marker="o")
             ax[x, y].plot([0,1,1,0,0],[0,0,1,1,0])
 
+bin_points = [(bins[i+1]+bins[i])/2 for i in range(20)]
 for i in range(15):
-    ax[i+1, i].plot(bins, hist_data[i,:])
+    ax[i+1, i].plot(bin_points, hist_data[i,:])
     peak_val = np.max(hist_data[i,:])
     ax[i+1, i].plot([true_param[i], true_param[i]], [0, peak_val], c='black')
     # ax[i+1, i].plot([accepted_mean[i], accepted_mean[i]], [0, peak_val], c='red')
     ax[i+1, i].plot([data_pred[i], data_pred[i]], [0, peak_val], c='gray')
-    ax[i+2, i].plot(bins, hist_data_add[i,:])
+    ax[i+2, i].plot(bin_points, hist_data_add[i,:])
     peak_val = np.max(hist_data_add[i,:])
 
     ax[i + 2, i].plot([true_param[i], true_param[i]], [0, peak_val], c='black')
