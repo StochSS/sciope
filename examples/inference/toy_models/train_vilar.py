fbb754405680115d66f8ff5f7b6f272696777f98
Mattias <ma10s@hotmail.com>
2019-08-23 16:42:21 +0200

    adjusting the subplots

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -21,7 +21,7 @@ from load_data import load_spec
 # validation_thetas = np.array(prior(n=10000))
 # validation_ts = np.expand_dims(np.array([simulate(p,n=100) for p in validation_thetas]),2)
 print("update")
-modelname = "vilar_ACR_200_401"
+modelname = "vilar_ACR_200_801"
 dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
 # train_thetas = pickle.load(open('datasets/' + modelname + '/train_thetas.p', "rb" ) )
@@ -47,7 +47,7 @@ print("training_thetas shape: ", train_thetas.shape)
 print("training_ts shape: ", train_ts.shape)
 
 # choose neural network model
-nnm = CNNModel(input_shape=(401,3), output_shape=(15))
+nnm = CNNModel(input_shape=(801,3), output_shape=(15))
 # nnm = PEN_CNNModel(input_shape=(401,3), output_shape=(15), pen_nr=10)
 # nm = ANNModel(input_shape=(100,1), output_shape=(2))
 
