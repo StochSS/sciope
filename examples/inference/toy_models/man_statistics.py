db5863ef2b155aba25e2c67fc803e07b6db3689d
Mattias <ma10s@hotmail.com>
2019-08-28 15:33:58 +0200

    adjusting the subplots

--- examples/inference/toy_models/man_statistics.py
+++ examples/inference/toy_models/man_statistics.py
@@ -9,12 +9,14 @@ def man_statistics(ts):
 def peak_finder(ts):
     len = ts.shape[0]
     print("len: ", len)
+    peaks = []
+
     for i in range(10,len-10):
-        peaks=[]
+
         print(i, ", ts[i]: ", ts[i], "max: ", np.max(ts[i-10:i+11]))
-        if ts[i]>=np.max(ts[i-10:i+11]):
+        if ts[i]>np.max(ts[i-10:i]) and ts[i]>np.max(ts[i+1:i+11]):
             print("peak found at: ", i)
-            peaks = peaks.append(i)
+            peaks.append(i)
             print("peaks: ", peaks)
-    print("peaks: ", peaks)
+    print("peaks found: ", peaks)
     return peaks
