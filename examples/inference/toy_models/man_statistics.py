9f7fac845fb40457d8424ab6dc2b425d9e3006e9
Mattias <ma10s@hotmail.com>
2019-08-28 15:46:10 +0200

    adjusting the subplots

--- examples/inference/toy_models/man_statistics.py
+++ examples/inference/toy_models/man_statistics.py
@@ -12,8 +12,8 @@ def peak_finder(ts):
 
     for i in range(10,len-10):
         treshold_peak = np.max(ts)*0.5
-        filter_start = np.maximum(0,i-20)
-        filter_end = np.maximum(len,i+21)
+        filter_start = np.maximum(0,i-10)
+        filter_end = np.maximum(len,i+11)
 
         if ts[i]>np.max(ts[filter_start:i]) and ts[i]>np.max(ts[i+1:filter_end]) and ts[i]>treshold_peak:
             peaks.append(i)
