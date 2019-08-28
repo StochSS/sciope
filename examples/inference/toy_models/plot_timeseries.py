c34bcc924b1cd058e5460e4af6656366ec5f3259
Mattias <ma10s@hotmail.com>
2019-08-28 14:10:12 +0200

    adjusting the subplots

--- examples/inference/toy_models/plot_timeseries.py
+++ examples/inference/toy_models/plot_timeseries.py
@@ -58,6 +58,7 @@ print("data shape: ", data.shape)
 f, ax = plt.subplots(2,1,figsize=(30,30))# ,sharex=True,sharey=True)
 f.suptitle('',fontsize=16)
 for ts in data[0,:,:].T:
+    print("ts shape: ", ts.shape)
     ax[0].plot(ts)
 
 plt.savefig('data_plots')
