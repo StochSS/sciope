615ddf23b17d20cc03b37bddf58f2b5477c9620a
Mattias <ma10s@hotmail.com>
2019-09-03 19:06:07 +0200

    adding cnn_regression_normal

--- examples/inference/toy_models/test_model.py
+++ examples/inference/toy_models/test_model.py
@@ -37,7 +37,7 @@ model.compile(optimizer=keras.optimizers.Adam(0.001), loss='mse')
 model.fit(x, y, epochs=5)#, verbose=False)
 
 # Make predictions.
-yhat = model(x_tst).eval()
+yhat = model(x_tst).mean()
 
 print("type yhat: ", type(yhat))
 print("shape yhat: ", yhat.shape)
