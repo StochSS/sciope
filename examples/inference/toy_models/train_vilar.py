c6f8b0f41812f237db31f96c574fa1691adf53ab
Mattias <ma10s@hotmail.com>
2019-09-13 09:14:11 +0200

    updates

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -19,7 +19,7 @@ from load_data import load_spec
 
 
 modelname = "vilar_ACR_200_401"
-parameter range
+# parameter range
 dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
 
