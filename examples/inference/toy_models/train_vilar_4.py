a65e688e96bddd3c5ab671159c14f9073cb9fd78
Mattias <ma10s@hotmail.com>
2019-09-15 00:44:12 +0200

    updates

--- examples/inference/toy_models/train_vilar_4.py
+++ examples/inference/toy_models/train_vilar_4.py
@@ -47,13 +47,13 @@ nnm = CNNModel(input_shape=(ts_len,3), output_shape=(15), con_len=2, con_layers=
 # nnm = PEN_CNNModel(input_shape=(ts_len,3), output_shape=(15), pen_nr=10)
 # nnm = ANNModel(input_shape=(ts_len, 3), output_shape=(15))
 
-nnm.load_model()
+# nnm.load_model()
 start_time = time.time()
-# nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
-#           batch_size=32, epochs=40, plot_training_progress=False)
-#
-# nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
-#           batch_size=4096, epochs=5, plot_training_progress=False)
+nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
+          batch_size=32, epochs=40, plot_training_progress=False)
+
+nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
+          batch_size=4096, epochs=5, plot_training_progress=False)
 end_time = time.time()
 training_time = end_time - start_time
 validation_pred = nnm.predict(validation_ts)
