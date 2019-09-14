27fd6e9a2c465e3bfae5f50e36f9de9ebcd10b4b
Mattias <ma10s@hotmail.com>
2019-09-14 11:40:01 +0200

    updates

--- examples/inference/toy_models/generate_data_vilar_prior2.py
+++ examples/inference/toy_models/generate_data_vilar_prior2.py
@@ -83,7 +83,7 @@ true_params = [[50.0, 500.0, 0.01, 50.0, 50.0, 5.0, 10.0, 0.5, 1.0, 0.2, 1.0, 1.
 obs_data = np.zeros((20,num_timestamps,3))
 for i in range(20):
     obs_data[i,:,:] = simulate(np.array(true_params))
-pickle.dump( true_params, open( 'datasets/' + modelname + '/true_param_pack.p', "wb" ) )
+pickle.dump( true_params, open( 'datasets/' + modelname + '/true_param.p', "wb" ) )
 pickle.dump( obs_data, open( 'datasets/' + modelname + '/obs_data_pack.p', "wb" ) )
 
 data = simulate(np.array(true_params))
