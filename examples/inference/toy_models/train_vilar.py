91150efbdbba7aee1d476d3ef97e6aff337e1af6
Mattias <ma10s@hotmail.com>
2019-09-18 14:32:53 +0200

    ...

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -37,17 +37,17 @@ validation_thetas = normalize_data(validation_thetas,dmin,dmax)
 
 ts_len = train_ts.shape[1]
 # choose neural network model
-nnm = CNNModel(input_shape=(ts_len,3), output_shape=(15), con_len=3, con_layers=[32,48,64,96])
-# nnm = PEN_CNNModel(input_shape=(ts_len,3), output_shape=(15), pen_nr=10)
+# nnm = CNNModel(input_shape=(ts_len,3), output_shape=(15), con_len=3, con_layers=[32,48,64,96])
+nnm = PEN_CNNModel(input_shape=(ts_len,3), output_shape=(15), pen_nr=10)
 # nnm = ANNModel(input_shape=(ts_len, 3), output_shape=(15))
 
-nnm.load_model()
+# nnm.load_model()
 start_time = time.time()
-# nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
-#           batch_size=32, epochs=40, plot_training_progress=False)
-#
-# nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
-#           batch_size=4096, epochs=5, plot_training_progress=False)
+nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
+          batch_size=32, epochs=40, plot_training_progress=False)
+
+nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
+          batch_size=4096, epochs=5, plot_training_progress=False)
 end_time = time.time()
 training_time = end_time - start_time
 validation_pred = nnm.predict(validation_ts)
