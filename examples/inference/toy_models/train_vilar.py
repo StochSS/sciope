5c103ad19bfe7ae05fed8f3743b0c6bac312c722
Mattias <ma10s@hotmail.com>
2019-09-20 08:42:31 +0200

    ...

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -39,7 +39,7 @@ ts_len = train_ts.shape[1]
 # choose neural network model
 # nnm = CNNModel(input_shape=(ts_len,3), output_shape=(15), con_len=3, con_layers=clay)
 # nnm = PEN_CNNModel(input_shape=(ts_len,3), output_shape=(15), pen_nr=10)
-nnm = ANNModel(input_shape=(ts_len, 3), output_shape=(15), layers=[100,100,100])
+nnm = ANNModel(input_shape=(ts_len, 3), output_shape=(15), layers=[200,200,200])
 print("Model name: ", nnm.name)
 
 # nnm.load_model('saved_models/None_DNNModel')
