c885bbcc50a0c3e37f9701d686ec09bbcdb6665d
Mattias <ma10s@hotmail.com>
2019-09-23 13:53:25 +0200

    ...

--- sciope/models/cnn_regressor.py
+++ sciope/models/cnn_regressor.py
@@ -31,7 +31,7 @@ class CNNModel(ModelBase):
     def train(self, inputs, targets,validation_inputs,validation_targets, batch_size, epochs, learning_rate=0.001,
               save_model = True, val_freq=1, early_stopping_patience=5, plot_training_progress=False):
 
-        es = keras.callbacks.EarlyStopping(monitor='val_mse', mode='min', verbose=1,patience=early_stopping_patience)
+        es = keras.callbacks.EarlyStopping(monitor='val_mean_absolute_error', mode='min', verbose=1,patience=early_stopping_patience)
 
         if save_model:
             mcp_save = keras.callbacks.ModelCheckpoint(self.save_as+'.hdf5',
