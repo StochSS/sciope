d93d0b089c99421ba1bfd7c17b51779bf6c4ed15
Mattias <ma10s@hotmail.com>
2019-09-27 12:47:11 +0200

    ...

--- examples/inference/toy_models/train_vilar_all_species.py
+++ examples/inference/toy_models/train_vilar_all_species.py
@@ -27,7 +27,7 @@ modelname = "vilar_ACR_" + str(endtime) + "_" + str(num_timestamps) + '_all_spec
 dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
 
-species = [1,2,3]
+species = [4,5,6]
 
 #Load data
 train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
