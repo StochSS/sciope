d57af68fdb7fcdad3e92a6c97e9c8b46f2a070a5
Mattias <ma10s@hotmail.com>
2019-09-28 09:02:08 +0200

    ...

--- sciope/models/pen_regressor_beta.py
+++ sciope/models/pen_regressor_beta.py
@@ -49,7 +49,7 @@ class PEN_CNNModel(ModelBase):
         history = self.model.fit(
             inputs, targets, validation_data=(validation_inputs,
                                               validation_targets), epochs=epochs, batch_size=batch_size, shuffle=True,
-            callbacks=[mcp_save, es], validation_freq=val_freq)
+            callbacks=[mcp_save, es])
 
         #To avoid overfitting load the model with best validation results after 
         #the first training part.        
