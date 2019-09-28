6ab9c4a6d98e82251619fdbe46bc47f1545b5efa
Mattias <ma10s@hotmail.com>
2019-09-28 17:18:47 +0200

    ...

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -48,139 +48,139 @@ clay=[32,48,64,96]
 ts_len = train_ts.shape[1]
 # choose neural network model
 nnm = CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), con_len=3, con_layers=clay)
-# nnm = PEN_CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), pen_nr=10)
-# nnm = ANNModel(input_shape=(ts_len, train_ts.shape[2]), output_shape=(15), layers=[200,200,200])
+# nnm = PEN_CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), pen_nr=10, con_layers=[50, 100, 200, 400])
+# nnm = ANNModel(input_shape=(ts_len, train_ts.shape[2]), output_shape=(15), layers=[200,200,00])
 print("Model name: ", nnm.name)
 
 # nnm.load_model('saved_models/None_DNNModel')
-start_time = time.time()
-history1 = nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
-          batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False)
-
-
-print("history: ", history1.history)
-pickle.dump(history1.history, open('history1.p', "wb"))
-
-
-history2 = nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
-          batch_size=4096, epochs=5*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False)
-
-
-
-
-end_time = time.time()
-training_time = end_time - start_time
-validation_pred = nnm.predict(validation_ts)
-validation_pred = np.reshape(validation_pred,(-1,15))
-train_pred = nnm.predict(train_ts)
-train_pred = np.reshape(train_pred,(-1,15))
-print("training time: ", training_time)
-print("validation mean square error: ", np.mean((validation_thetas-validation_pred)**2))
-print("validation mean absolute error: ", np.mean(abs(validation_thetas-validation_pred)))
-print("training mean absolute error: ", np.mean(abs(train_thetas-train_pred)))
-
-validation_mae = np.mean(abs(validation_thetas-validation_pred), axis=0)
-
-para_names = vilar.get_parameter_names()
-
-for i in range(15):
-    pk = para_names[i]
-    pks = pk.split("_")
-    if len(pks) > 1:
-        pk_p = "\hat{\\" + pks[0].lower() + "}_{" + pks[1].upper() + "}"
-        pk = pks[0].lower() + "_{" + pks[1].upper() + "}"
-    if len(pks) == 3:
-        print("len 3: ", pks[2])
-        if pks[2] == 'prime':
-            pk_p = pk_p + "'"
-            pk = pk + "'"
-
-    para_name_p = "$" + pk_p + "$"
-    para_names[i] = "$\\" + pk + "$"
-
-validation_thetas = denormalize_data(validation_thetas, dmin, dmax)
-validation_pred = denormalize_data(validation_pred, dmin, dmax)
-
-mean_dev = np.mean(abs(validation_thetas-validation_pred), axis=0)
-
-
-
-i=0
-for dev, rdev, n in zip(mean_dev,validation_mae ,para_names):
-    print(n, " mean deviation: ", "{0:.4f}".format(dev), " real mean deviation: ", "{0:.4f}".format(rdev), ", range: ", dmin[i], " - ", dmax[i])
-    i+=1
-
-
-true_param = pickle.load(open('datasets/' + modelname + '/true_param.p', "rb" ) )
-true_param = np.squeeze(np.array(true_param))
-print("true_param shape: ", true_param.shape)
-num_timestamps=401
-endtime=200
-
-# true_param = np.ones((15))*0.8
-
-
-Vilar_ = Vilar_model(num_timestamps=num_timestamps, endtime=endtime)
-
-
-simulate = Vilar_.simulate
-print("before simulation")
-data = np.array([np.squeeze(simulate(true_param)) for i in range(100)])
-data = data[:,:end_step:step,species]
-data_pred = nnm.predict(data)
-data_pred = np.squeeze(data_pred)
-data_pred_denorm = denormalize_data(data_pred,dmin,dmax)
-
-data_pred_meandev = np.mean( abs(data_pred_denorm- true_param), axis=0)
-
-true_param = np.squeeze(np.array(true_param))
-# print("true_param shape: ", true_param.shape)
+# start_time = time.time()
+# history1 = nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
+#           batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False)
 #
-# print("abs(data_pred_denorm - true_param) = ", abs(data_pred_denorm - true_param) )
-
-rel_e1 = np.mean(abs(data_pred_denorm - true_param),axis=0) / np.mean(abs( (np.array(dmin)+np.array(dmax))/2 - true_param),axis=0)
-
-print("rel_e1 shape: ", rel_e1.shape)
-
-i=0
-for dev, re, n in zip(data_pred_meandev,rel_e1,para_names):
-    print(n, ", true: ", true_param[i], ", predicted: ", "{0:.4f}".format(data_pred_denorm[0,i]), ", mean deviation: ",
-          "{0:.4f}".format(dev), ", rel dev: ", "{0:.4f}".format(re), ", range: ", dmin[i], " - ", dmax[i])
-    i+=1
-
-
-# nnm.load_model()
-# validation_pred = np.array([nnm.predict(validation_ts[i*100:(i+1)*100]) for i in range(500)])
-
-
-test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
-test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )
-test_ts = test_ts[:,:end_step:step,species]
-test_thetas_n = normalize_data(test_thetas,dmin,dmax)
-test_pred = nnm.predict(test_ts)
-test_pred = np.reshape(test_pred,(-1,15))
-test_pred_d = denormalize_data(test_pred,dmin,dmax)
-test_mse = np.mean((test_thetas-test_pred)**2)
-test_mae = np.mean(abs(test_thetas-test_pred_d))
-test_ae = np.mean(abs(test_thetas-test_pred_d),axis=0)
-test_ae_norm = np.mean(abs(test_thetas_n-test_pred),axis=0)
-
-rel_e = np.mean(abs(test_thetas-test_pred),axis=0) / np.mean(abs(1/2 - test_thetas),axis=0)
-print("rel_e shape: ", rel_e.shape)
-
-
-i=0
-for dev, re, n in zip(test_ae, test_ae_norm, para_names):
-    print(n, " mean deviation: ", "{0:.4f}".format(dev), ", rel dev: ", "{0:.4f}".format(re), ", range: ", dmin[i], " - ", dmax[i])
-    i+=1
-
-
-print("Model name: ", nnm.name)
-print("mean square error: ", test_mse)
-print("mean absolute error: ", test_mae)
-
-test_results = {"model name": nnm.name, "training_time": training_time, "mse": test_mse, "mae": test_mae, "ae": test_ae, "rel_e": rel_e, "rel_test_ae": test_ae_norm}
-pickle.dump(test_results,
-            open('results/training_results_' + modelname + '_' + nnm.name + '_training_size_' + str(training_size) +
-                 '_step_' + str(step) + '_endstep_' + str(end_step) + '_species_' + str(species) + '.p', "wb"))
-
+#
+# print("history: ", history1.history)
+# pickle.dump(history1.history, open('history1.p', "wb"))
+#
+#
+# history2 = nnm.train(inputs=train_ts, targets=train_thetas,validation_inputs=validation_ts,validation_targets=validation_thetas,
+#           batch_size=4096, epochs=5*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False)
+#
+#
+#
+#
+# end_time = time.time()
+# training_time = end_time - start_time
+# validation_pred = nnm.predict(validation_ts)
+# validation_pred = np.reshape(validation_pred,(-1,15))
+# train_pred = nnm.predict(train_ts)
+# train_pred = np.reshape(train_pred,(-1,15))
+# print("training time: ", training_time)
+# print("validation mean square error: ", np.mean((validation_thetas-validation_pred)**2))
+# print("validation mean absolute error: ", np.mean(abs(validation_thetas-validation_pred)))
+# print("training mean absolute error: ", np.mean(abs(train_thetas-train_pred)))
+#
+# validation_mae = np.mean(abs(validation_thetas-validation_pred), axis=0)
+#
+# para_names = vilar.get_parameter_names()
+#
+# for i in range(15):
+#     pk = para_names[i]
+#     pks = pk.split("_")
+#     if len(pks) > 1:
+#         pk_p = "\hat{\\" + pks[0].lower() + "}_{" + pks[1].upper() + "}"
+#         pk = pks[0].lower() + "_{" + pks[1].upper() + "}"
+#     if len(pks) == 3:
+#         print("len 3: ", pks[2])
+#         if pks[2] == 'prime':
+#             pk_p = pk_p + "'"
+#             pk = pk + "'"
+#
+#     para_name_p = "$" + pk_p + "$"
+#     para_names[i] = "$\\" + pk + "$"
+#
+# validation_thetas = denormalize_data(validation_thetas, dmin, dmax)
+# validation_pred = denormalize_data(validation_pred, dmin, dmax)
+#
+# mean_dev = np.mean(abs(validation_thetas-validation_pred), axis=0)
+#
+#
+#
+# i=0
+# for dev, rdev, n in zip(mean_dev,validation_mae ,para_names):
+#     print(n, " mean deviation: ", "{0:.4f}".format(dev), " real mean deviation: ", "{0:.4f}".format(rdev), ", range: ", dmin[i], " - ", dmax[i])
+#     i+=1
+#
+#
+# true_param = pickle.load(open('datasets/' + modelname + '/true_param.p', "rb" ) )
+# true_param = np.squeeze(np.array(true_param))
+# print("true_param shape: ", true_param.shape)
+# num_timestamps=401
+# endtime=200
+#
+# # true_param = np.ones((15))*0.8
+#
+#
+# Vilar_ = Vilar_model(num_timestamps=num_timestamps, endtime=endtime)
+#
+#
+# simulate = Vilar_.simulate
+# print("before simulation")
+# data = np.array([np.squeeze(simulate(true_param)) for i in range(100)])
+# data = data[:,:end_step:step,species]
+# data_pred = nnm.predict(data)
+# data_pred = np.squeeze(data_pred)
+# data_pred_denorm = denormalize_data(data_pred,dmin,dmax)
+#
+# data_pred_meandev = np.mean( abs(data_pred_denorm- true_param), axis=0)
+#
+# true_param = np.squeeze(np.array(true_param))
+# # print("true_param shape: ", true_param.shape)
+# #
+# # print("abs(data_pred_denorm - true_param) = ", abs(data_pred_denorm - true_param) )
+#
+# rel_e1 = np.mean(abs(data_pred_denorm - true_param),axis=0) / np.mean(abs( (np.array(dmin)+np.array(dmax))/2 - true_param),axis=0)
+#
+# print("rel_e1 shape: ", rel_e1.shape)
+#
+# i=0
+# for dev, re, n in zip(data_pred_meandev,rel_e1,para_names):
+#     print(n, ", true: ", true_param[i], ", predicted: ", "{0:.4f}".format(data_pred_denorm[0,i]), ", mean deviation: ",
+#           "{0:.4f}".format(dev), ", rel dev: ", "{0:.4f}".format(re), ", range: ", dmin[i], " - ", dmax[i])
+#     i+=1
+#
+#
+# # nnm.load_model()
+# # validation_pred = np.array([nnm.predict(validation_ts[i*100:(i+1)*100]) for i in range(500)])
+#
+#
+# test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
+# test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )
+# test_ts = test_ts[:,:end_step:step,species]
+# test_thetas_n = normalize_data(test_thetas,dmin,dmax)
+# test_pred = nnm.predict(test_ts)
+# test_pred = np.reshape(test_pred,(-1,15))
+# test_pred_d = denormalize_data(test_pred,dmin,dmax)
+# test_mse = np.mean((test_thetas-test_pred)**2)
+# test_mae = np.mean(abs(test_thetas-test_pred_d))
+# test_ae = np.mean(abs(test_thetas-test_pred_d),axis=0)
+# test_ae_norm = np.mean(abs(test_thetas_n-test_pred),axis=0)
+#
+# rel_e = np.mean(abs(test_thetas-test_pred),axis=0) / np.mean(abs(1/2 - test_thetas),axis=0)
+# print("rel_e shape: ", rel_e.shape)
+#
+#
+# i=0
+# for dev, re, n in zip(test_ae, test_ae_norm, para_names):
+#     print(n, " mean deviation: ", "{0:.4f}".format(dev), ", rel dev: ", "{0:.4f}".format(re), ", range: ", dmin[i], " - ", dmax[i])
+#     i+=1
+#
+#
+# print("Model name: ", nnm.name)
+# print("mean square error: ", test_mse)
+# print("mean absolute error: ", test_mae)
+#
+# test_results = {"model name": nnm.name, "training_time": training_time, "mse": test_mse, "mae": test_mae, "ae": test_ae, "rel_e": rel_e, "rel_test_ae": test_ae_norm}
+# pickle.dump(test_results,
+#             open('results/training_results_' + modelname + '_' + nnm.name + '_training_size_' + str(training_size) +
+#                  '_step_' + str(step) + '_endstep_' + str(end_step) + '_species_' + str(species) + '.p', "wb"))
+#
