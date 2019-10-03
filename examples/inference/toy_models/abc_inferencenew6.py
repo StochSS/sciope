f101ca92cae43c1d2e92075ccf78556596381015
Mattias <ma10s@hotmail.com>
2019-10-03 19:34:54 +0200

    ...

--- examples/inference/toy_models/abc_inferencenew6.py
+++ examples/inference/toy_models/abc_inferencenew6.py
@@ -43,7 +43,7 @@ true_param = pickle.load(open('datasets/' + modelname + '/true_param.p', "rb" ) 
 true_param = np.squeeze(np.array(true_param))
 print("true_param shape: ", true_param.shape)
 data = pickle.load(open('datasets/' + modelname + '/obs_data_pack.p', "rb" ) )
-data = data[1]
+data = data[2]
 data = np.expand_dims(data,0)
 print("data shape: ", data.shape)
 # data_exp = np.expand_dims( np.expand_dims(data,axis=0), axis=2 )
