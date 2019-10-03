7db9ba78c71e6a070140014146ac5e3a876d164a
Mattias <ma10s@hotmail.com>
2019-10-03 20:15:09 +0200

    ...

--- examples/inference/toy_models/abc_inferencenew6.py
+++ examples/inference/toy_models/abc_inferencenew6.py
@@ -209,14 +209,14 @@ for x in range(15):
 
 
             ax[x, y].tick_params(labelleft=False)
-            ax[y, x].plot(l, p, c='green', lw=lwith)
-            ax[y, x].plot(l, p_full, c='red', lw=lwith, ls='--')
+            # ax[y, x].plot(l, p, c='green', lw=lwith)
+            # ax[y, x].plot(l, p_full, c='red', lw=lwith, ls='--')
             ax[x, y].set_xlabel(para_names[x], fontsize=fsize)
             # ax[x, y].yaxis.set_label_position("left")
             ax[x, y].set_ylabel('density', fontsize=fsize, rotation=90)
 
-            ret = ax[x, y].hist(accepted_para[:, x], density=True, bins=30, color='green', alpha=0.5)
-            ax[x, y].hist(accepted_para_full[:, x], density=True, bins=30, color='red', alpha=0.1)
+            ret = ax[x, y].hist(accepted_para[:, x], density=True, bins=30, color='green', alpha=1)
+            ax[x, y].hist(accepted_para_full[:, x], density=True, bins=30, color='red', alpha=0.3)
 
             peak_val = np.maximum(np.max(ret[0]), np.max(p))
             ax[x, y].plot([true_param[x], true_param[x]], [0, peak_val], c='black')
