35d4ed42ec9106babcc95c60527b2aceff9ff8ae
Mattias <ma10s@hotmail.com>
2019-10-05 22:42:30 +0200

    ...

--- examples/inference/toy_models/accuracy_plot6.py
+++ examples/inference/toy_models/accuracy_plot6.py
@@ -53,9 +53,11 @@ print("data shape: ", data.shape)
 data_pred = nnm.predict(data)
 data_pred_denorm = denormalize_data(data_pred,dmin,dmax)
 
-test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
-# abc_trial_thetas = normalize_data(abc_trial_thetas,dmin,dmax)
-test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )
+# test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
+# test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )
+
+test_thetas, test_ts = load_spec(modelname=modelname, type = "train")
+
 
 test_thetas_min = np.min(test_thetas,0)
 test_thetas_min_text = ["{0:.1f}".format(s) for s in test_thetas_min]
