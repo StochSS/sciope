8a1ded4c9700f2662400b03a992a042d1c0aa183
Mattias <ma10s@hotmail.com>
2019-10-08 13:20:07 +0200

    ...

--- examples/inference/toy_models/train_vilar_all_species.py
+++ examples/inference/toy_models/train_vilar_all_species.py
@@ -60,6 +60,11 @@ nnm = CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), con_le
 
 # nnm = ANNModel(input_shape=(ts_len, 3), output_shape=(15), layers=[200,200,200])
 print("Model name: ", nnm.name)
+para_names = vilar.get_parameter_names()
+
+print("parameter names: ")
+for p in para_names:
+    print(p)
 
 # nnm.load_model()
 start_time = time.time()
@@ -84,64 +89,13 @@ print("validation mean square error: ", np.mean((validation_thetas-validation_pr
 print("validation mean absolute error: ", np.mean(abs(validation_thetas-validation_pred)))
 print("training mean absolute error: ", np.mean(abs(train_thetas-train_pred)))
 
-validation_mae = np.mean(abs(validation_thetas-validation_pred), axis=0)
-
-para_names = vilar.get_parameter_names()
-
-
-validation_thetas = denormalize_data(validation_thetas, dmin, dmax)
-validation_pred = denormalize_data(validation_pred, dmin, dmax)
-
-mean_dev = np.mean(abs(validation_thetas-validation_pred), axis=0)
 
 
 
-i=0
-for dev, rdev, n in zip(mean_dev,validation_mae ,para_names):
-    print(n, " mean deviation: ", "{0:.4f}".format(dev), " real mean deviation: ", "{0:.4f}".format(rdev), ", range: ", dmin[i], " - ", dmax[i])
-    i+=1
 
 
-true_param = pickle.load(open('datasets/' + modelname + '/true_param.p', "rb" ) )
-true_param = np.squeeze(np.array(true_param))
-print("true_param shape: ", true_param.shape)
-num_timestamps=401
-endtime=200
-
-# true_param = np.ones((15))*0.8
 
 
-Vilar_ = Vilar_model(num_timestamps=num_timestamps, endtime=endtime)
-
-
-simulate = Vilar_.simulate
-print("before simulation")
-data = np.array([np.squeeze(simulate(true_param)) for i in range(100)])
-data = data[:,:end_step:step,species]
-data_pred = nnm.predict(data)
-data_pred = np.squeeze(data_pred)
-data_pred_denorm = denormalize_data(data_pred,dmin,dmax)
-
-data_pred_meandev = np.mean( abs(data_pred_denorm- true_param), axis=0)
-
-true_param = np.squeeze(np.array(true_param))
-# print("true_param shape: ", true_param.shape)
-#
-# print("abs(data_pred_denorm - true_param) = ", abs(data_pred_denorm - true_param) )
-
-rel_e1 = np.mean(abs(data_pred_denorm - true_param),axis=0) / np.mean(abs( (np.array(dmin)+np.array(dmax))/2 - true_param),axis=0)
-
-print("rel_e1 shape: ", rel_e1.shape)
-
-i=0
-for dev, re, n in zip(data_pred_meandev,rel_e1,para_names):
-    print(n, ", true: ", true_param[i], ", predicted: ", "{0:.4f}".format(data_pred_denorm[0,i]), ", mean deviation: ",
-          "{0:.4f}".format(dev), ", rel dev: ", "{0:.4f}".format(re), ", range: ", dmin[i], " - ", dmax[i])
-    i+=1
-
-
-# nnm.load_model()
-# validation_pred = np.array([nnm.predict(validation_ts[i*100:(i+1)*100]) for i in range(500)])
 
 
 test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
@@ -156,21 +110,14 @@ test_mae = np.mean(abs(test_thetas-test_pred_d))
 test_ae = np.mean(abs(test_thetas-test_pred_d),axis=0)
 test_ae_norm = np.mean(abs(test_thetas_n-test_pred),axis=0)
 
-rel_e = np.mean(abs(test_thetas-test_pred),axis=0) / np.mean(abs(1/2 - test_thetas),axis=0)
-print("rel_e shape: ", rel_e.shape)
 
-
-i=0
-for dev, re, n in zip(test_ae, test_ae_norm, para_names):
-    print(n, " mean deviation: ", "{0:.4f}".format(dev), ", rel dev: ", "{0:.4f}".format(re), ", range: ", dmin[i], " - ", dmax[i])
-    i+=1
 
 
 print("Model name: ", nnm.name)
 print("mean square error: ", test_mse)
 print("mean absolute error: ", test_mae)
 
-test_results = {"model name": nnm.name, "training_time": training_time, "mse": test_mse, "mae": test_mae, "ae": test_ae, "rel_e": rel_e, "rel_test_ae": test_ae_norm}
+test_results = {"model name": nnm.name, "training_time": training_time, "mse": test_mse, "mae": test_mae, "ae": test_ae, "rel_test_ae": test_ae_norm}
 pickle.dump(test_results,
             open('results/training_results_' + modelname + '_' + nnm.name + '_training_size_' + str(training_size) +
                  '_step_' + str(step) + '_endstep_' + str(end_step) + '_species_' + str(species) + '.p', "wb"))
