c889840e35c42feb50eec549704c9d00e936dd46
Mattias <ma10s@hotmail.com>
2019-10-09 16:27:26 +0200

    ...

--- examples/inference/toy_models/train_vilar_all_species.py
+++ examples/inference/toy_models/train_vilar_all_species.py
@@ -28,7 +28,7 @@ dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
 dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
 
 #5,6,7,8
-species = [4]
+species = [5]
 
 #Load data
 train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
