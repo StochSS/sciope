518c9126c8a7a10f243087c21d0956d0253c906a
Mattias <ma10s@hotmail.com>
2019-10-11 12:55:20 +0200

    ...

--- examples/inference/toy_models/train_vilar_all_species.py
+++ examples/inference/toy_models/train_vilar_all_species.py
@@ -59,6 +59,7 @@ print("species: ", species)
 # choose neural network model
 # nnm = CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), con_len=3, con_layers=clay)
 # nnm = CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), con_len=3, con_layers=clay, dense_layers=[100,100,100])
+nnm = ANNModel(input_shape=(ts_len, train_ts.shape[2]), output_shape=(15), layers=[100,100,100])
 
 nnm = PEN_CNNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(15), pen_nr=10, con_layers=clay, dense_layers=[100,100,100])
 print("save results as: ", 'results/training_results_' + modelname + '_' + nnm.name + '_training_size_' + str(training_size) +
