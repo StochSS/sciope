1e8333410a90088d927cb3e8815aa7bcab22dd63
Mattias <ma10s@hotmail.com>
2019-10-11 09:42:31 +0200

    ...

--- examples/inference/toy_models/train_vilar_all_species.py
+++ examples/inference/toy_models/train_vilar_all_species.py
@@ -31,6 +31,9 @@ dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
 #6=C, 7=A, 8=R
 species = [6]
 
+
+
+
 #Load data
 train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
 print("load train data done!")
@@ -58,7 +61,8 @@ print("species: ", species)
 # nnm = CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), con_len=3, con_layers=clay, dense_layers=[100,100,100])
 
 nnm = PEN_CNNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(15), pen_nr=10, con_layers=clay, dense_layers=[100,100,100])
-
+print("save results as: ", 'results/training_results_' + modelname + '_' + nnm.name + '_training_size_' + str(training_size) +
+                 '_step_' + str(step) + '_endstep_' + str(end_step) + '_species_' + str(species) + '.p', "wb")
 # nnm = ANNModel(input_shape=(ts_len, 3), output_shape=(15), layers=[200,200,200])
 print("Model name: ", nnm.name)
 para_names = vilar.get_parameter_names()
