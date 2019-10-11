30ed63a406559e260b2545f5d5ba1acafc242af8
Mattias <ma10s@hotmail.com>
2019-10-11 22:48:16 +0200

    ...

--- examples/inference/toy_models/train_vilar.py
+++ examples/inference/toy_models/train_vilar.py
@@ -50,7 +50,7 @@ ts_len = train_ts.shape[1]
 # choose neural network model
 print("big neural net.")
 # nnm = CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=15, con_len=3, con_layers=clay, dense_layers=[500,500,500],dataname='vilar_prior1')
-nnm = PEN_CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), pen_nr=12, con_layers=[32,64,128], dense_layers=[100,100,100])
+nnm = PEN_CNNModel(input_shape=(ts_len,train_ts.shape[2]), output_shape=(15), pen_nr=12, con_layers=[32,64,128,256], dense_layers=[100,100,100])
 # nnm = ANNModel(input_shape=(ts_len, train_ts.shape[2]), output_shape=(15), layers=[200,200,00])
 print("Model name: ", nnm.name)
 verb = 2
