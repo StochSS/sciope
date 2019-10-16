4fa23b2fb5dea8321b7d3af3e024e5fb6ada1efe
Mattias <ma10s@hotmail.com>
2019-10-16 14:42:23 +0200

    ...

--- examples/inference/toy_models/train_scheme.py
+++ examples/inference/toy_models/train_scheme.py
@@ -2,7 +2,7 @@ from train_vilar_all_species_func import train_routine
 
 completed_subsets = [[0,1],[6,8],[5,7],[0,2]]
 
-for s1 in range(8):
+for s1 in range(7,8):
     for s2 in range(s1+1,9):
         if [s1,s2] not in completed_subsets:
             train_routine(species=[s1,s2])
