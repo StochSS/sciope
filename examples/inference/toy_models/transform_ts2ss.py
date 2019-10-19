1b9d5c65b8d9565b0d68a4d03f867ba2d705c78d
Mattias <ma10s@hotmail.com>
2019-10-19 12:51:51 +0200

    k

--- examples/inference/toy_models/transform_ts2ss.py
+++ examples/inference/toy_models/transform_ts2ss.py
@@ -13,25 +13,25 @@ endtime=200
 modelname = "vilar_ACR_" + str(endtime) + "_" + str(num_timestamps) + '_all_species'
 species = [6]
 
-train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
-validation_thetas = pickle.load(open('datasets/' + modelname + '/validation_thetas.p', "rb" ) )
-validation_ts = pickle.load(open('datasets/' + modelname + '/validation_ts.p', "rb" ) )[:,:,species]
+# train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
+# validation_thetas = pickle.load(open('datasets/' + modelname + '/validation_thetas.p', "rb" ) )
+# validation_ts = pickle.load(open('datasets/' + modelname + '/validation_ts.p', "rb" ) )[:,:,species]
 test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
-test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )
+test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )[:,:,species]
 
-print("data loaded")
-start = time.time()
-itt=0
-val_sum = np.array([summarys(validation_ts[ts],ts) for ts in range(len(validation_ts))])
-end = time.time()
-print("validation summarys generated in ", end-start)
-pickle.dump( val_sum, open( 'datasets/' + modelname + '/val_sum.p', "wb" ) )
-
-start = time.time()
-train_sum = np.array([summarys(train_ts[ts],ts) for ts in range(len(train_ts))])
-end = time.time()
-print("train summarys generated in ", end-start)
-pickle.dump( train_sum, open( 'datasets/' + modelname + '/train_sum.p', "wb" ) )
+# print("data loaded")
+# start = time.time()
+# itt=0
+# val_sum = np.array([summarys(validation_ts[ts],ts) for ts in range(len(validation_ts))])
+# end = time.time()
+# print("validation summarys generated in ", end-start)
+# pickle.dump( val_sum, open( 'datasets/' + modelname + '/val_sum.p', "wb" ) )
+#
+# start = time.time()
+# train_sum = np.array([summarys(train_ts[ts],ts) for ts in range(len(train_ts))])
+# end = time.time()
+# print("train summarys generated in ", end-start)
+# pickle.dump( train_sum, open( 'datasets/' + modelname + '/train_sum.p', "wb" ) )
 
 start = time.time()
 test_sum = np.array([summarys(test_ts[ts],ts) for ts in range(len(test_ts))])
