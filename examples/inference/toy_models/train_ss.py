3682939cd0029ebd2e874f08d5bfe2cc56254636
Mattias <ma10s@hotmail.com>
2019-10-19 16:29:25 +0200

    k

--- examples/inference/toy_models/train_ss.py
+++ examples/inference/toy_models/train_ss.py
@@ -7,8 +7,8 @@ import time
 
 from load_data import load_spec
 
-clay=[32,48,64]#,96]
-clay = [4,8,16]
+clay=[32,48,64,96]
+# clay = [4,8,16]
 num_timestamps=401
 endtime=200
 modelname = "vilar_ACR_" + str(endtime) + "_" + str(num_timestamps) + '_all_species'
@@ -22,13 +22,17 @@ test_sum = pickle.load(open('datasets/' + modelname + '/test_sum.p', "rb" ) )
 test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )[:,:,species]
 
 
-nnm = CNNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(2), con_len=19, con_layers=clay, dense_layers=[100,100,100])
+nnm = CNNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(2), con_len=3, con_layers=clay, dense_layers=[100,100,100])
 
 history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
           batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
 
 
 # nnm.load_model()
+
+train_pred = nnm.predict(train_ts)
+train_mae = np.mean(abs(train_pred-test_sum),axis=0)
+print("test_mae: ", train_mae)
 
 test_pred = nnm.predict(test_ts)
 test_mae = np.mean(abs(test_pred-test_sum),axis=0)
