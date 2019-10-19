9a6a31973d2440e9f03bf1b468a2f60afafa9fae
Mattias <ma10s@hotmail.com>
2019-10-19 15:04:28 +0200

    k

--- examples/inference/toy_models/train_ss.py
+++ examples/inference/toy_models/train_ss.py
@@ -17,13 +17,18 @@ train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=
 train_sum = pickle.load(open('datasets/' + modelname + '/train_sum.p', "rb" ) )
 validation_sum = pickle.load(open('datasets/' + modelname + '/val_sum.p', "rb" ) )
 validation_ts = pickle.load(open('datasets/' + modelname + '/validation_ts.p', "rb" ) )[:,:,species]
-test_thetas = pickle.load(open('datasets/' + modelname + '/test_sum.p', "rb" ) )
+test_sum = pickle.load(open('datasets/' + modelname + '/test_sum.p', "rb" ) )
 test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )[:,:,species]
 
 
 nnm = CNNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(2), con_len=19, con_layers=clay, dense_layers=[100,100,100])
 
-history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
-          batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
+# history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
+#           batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
+#
 
+nnm.load_model()
 
+test_pred = nnm.predict(test_ts)
+test_mae = np.mean(abs(test_pred-test_sum),axis=0)
+print("test_mae: ", test_mae)
