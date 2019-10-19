18952378cb87697ea8ad81f01b9f7c0281169c3b
Mattias <ma10s@hotmail.com>
2019-10-19 12:08:42 +0200

    k

--- examples/inference/toy_models/create_summary_statistics.py
+++ examples/inference/toy_models/create_summary_statistics.py
@@ -51,14 +51,14 @@ def summarys(ts,itt=-1):
     return np.array([m_dist,s_dist])
 
 
-validation_ts = pickle.load(open('validation_ts.p', "rb" ) )[:,:,species]
-
-print("data loaded")
-start = time.time()
-val_sum = np.array([summarys(ts) for ts in validation_ts])
-end = time.time()
-print("summarys generated in ", end-start)
-print(val_sum[200:210])
+# validation_ts = pickle.load(open('validation_ts.p', "rb" ) )[:,:,species]
+#
+# print("data loaded")
+# start = time.time()
+# val_sum = np.array([summarys(ts) for ts in validation_ts])
+# end = time.time()
+# print("summarys generated in ", end-start)
+# print(val_sum[200:210])
 
 # f,ax = plt.subplots(4)
 # j=0
