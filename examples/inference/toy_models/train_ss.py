76afc4f882a5ee5ff1c4e0c8b81f8d560c7f20c7
Mattias <ma10s@hotmail.com>
2019-10-20 19:11:40 +0200

    k

--- examples/inference/toy_models/train_ss.py
+++ examples/inference/toy_models/train_ss.py
@@ -24,7 +24,7 @@ test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )[:,:,
 print("train sum shape: ", train_sum.shape)
 print("train ts shape: ", train_ts.shape)
 
-train_ind = np.where(train_sum > 0)[0]
+train_ind = np.where(train_sum[:,0] >= 0)[0]
 print("train ind shape: ", np.array(train_ind).shape)
 train_sum = train_sum[train_ind]
 train_ts = train_ts[train_ind]
