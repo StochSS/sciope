1df9bbee3d497ad4abd5745ed16ad61a27153f5a
Mattias <ma10s@hotmail.com>
2019-10-20 19:15:53 +0200

    k

--- examples/inference/toy_models/train_ss.py
+++ examples/inference/toy_models/train_ss.py
@@ -31,41 +31,41 @@ train_ts = train_ts[train_ind]
 print("train sum shape: ", train_sum.shape)
 print("train ts shape: ", train_ts.shape)
 
-test_ind = np.where(test_sum > 0)[0]
+test_ind = np.where(test_sum[:,0] >= 0)[0]
 test_sum = test_sum[test_ind]
 test_ts = test_ts[test_ind]
 
-validation_ind = np.where(validation_sum > 0)[0]
+validation_ind = np.where(validation_sum[:,0] >= 0)[0]
 validation_sum = train_sum[validation_ind]
 validation_ts = validation_ts[validation_ind]
 
-# nnm = CNNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(2), con_len=3, con_layers=clay,
-#                dense_layers=[100,100,100], dataname='wozeros')
-#
-# # nnm.load_model()
-#
-#
-# history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
-#           batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
-#
-#
-#
-# history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
-#           batch_size=4096, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
-#
-#
-# train_pred = nnm.predict(train_ts)
-# train_mae = np.mean(abs(train_pred-train_sum),axis=0)
-# print("train_mae: ", train_mae)
-#
-# test_pred = nnm.predict(test_ts)
-# test_mae = np.mean(abs(test_pred-test_sum),axis=0)
-# print("test_mae: ", test_mae)
-#
-# print("test sum std: ", np.std(test_sum,0))
-# print("test sum mean: ", np.mean(test_sum,0))
-# print("optimal guessing deviation: ", np.mean(abs(test_sum - np.mean(test_sum,0)),0))
-#
-#
-# for i in range(25):
-#     print("test_sum: ", test_sum[i], ", test_pred: ", test_pred[i])
+nnm = CNNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(2), con_len=3, con_layers=clay,
+               dense_layers=[100,100,100], dataname='wozeros')
+
+# nnm.load_model()
+
+
+history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
+          batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
+
+
+
+history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
+          batch_size=4096, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
+
+
+train_pred = nnm.predict(train_ts)
+train_mae = np.mean(abs(train_pred-train_sum),axis=0)
+print("train_mae: ", train_mae)
+
+test_pred = nnm.predict(test_ts)
+test_mae = np.mean(abs(test_pred-test_sum),axis=0)
+print("test_mae: ", test_mae)
+
+print("test sum std: ", np.std(test_sum,0))
+print("test sum mean: ", np.mean(test_sum,0))
+print("optimal guessing deviation: ", np.mean(abs(test_sum - np.mean(test_sum,0)),0))
+
+
+for i in range(25):
+    print("test_sum: ", test_sum[i], ", test_pred: ", test_pred[i])
