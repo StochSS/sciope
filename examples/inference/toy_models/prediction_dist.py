7ddb50da870a6f1f1cab3f7b132ad1cb26f6d820
Mattias <ma10s@hotmail.com>
2019-10-21 18:11:36 +0200

    k

--- examples/inference/toy_models/prediction_dist.py
+++ examples/inference/toy_models/prediction_dist.py
@@ -76,7 +76,9 @@ f, ax = plt.subplots(3,5,figsize=(40,40))
 for x in range(3):
     for y in range(5):
         i = x*5+y
-        ax[x,y].hist2d(test_thetas[i],test_pred_d[i])
+        b = np.linspace(dmin[i], dmax[i], 11)
+        ax[x,y].hist2d(test_thetas[i],test_pred_d[i], bins=b)
+        plt.plot([dmin[i], dmin[i], dmax[i], dmax[i], dmin[i]], [dmin[i], dmax[i], dmax[i], dmin[i], dmin[i]], c='white')
 
 plt.savefig("heatmap_testset")
 
