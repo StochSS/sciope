6e6cb0f608f5c03d9c3cc8c41ccd2bf70d8d48ea
Mattias <ma10s@hotmail.com>
2019-10-21 10:38:48 +0200

    k

---
+++ examples/inference/toy_models/train_tsfresh.py
@@ -0,0 +1,71 @@
+from sciope.models.cnn_regressor import CNNModel
+from sciope.models.dnn_regressior import ANNModel
+from tsfresh import extract_features
+import numpy as np
+from create_summary_statistics import summarys
+import pickle
+import time
+
+from load_data import load_spec
+
+clay=[32,48,64,96]
+# clay = [4,8,16]
+num_timestamps=401
+endtime=200
+modelname = "vilar_ACR_" + str(endtime) + "_" + str(num_timestamps) + '_all_species'
+species = [6]
+
+train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
+validation_thetas = pickle.load(open('datasets/' + modelname + '/val_thetas.p', "rb" ) )
+validation_ts = pickle.load(open('datasets/' + modelname + '/validation_ts.p', "rb" ) )[:,:,species]
+test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
+test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )[:,:,species]
+
+obs_data = pickle.load(open('datasets/' + modelname + '/obs_data_pack.p', "rb" ) )[:,:,species]
+
+train_input = extract_features(train_ts)
+print("train_input_shape: ", train_input.shape)
+
+
+nnm = ANNModel(input_shape=(train_ts.shape[1],train_ts.shape[2]), output_shape=(2), con_len=3, con_layers=clay,
+               dense_layers=[100,100,100])#, dataname='wozeros')
+
+# nnm.load_model()
+
+
+# history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
+#           batch_size=32, epochs=20, val_freq=1, early_stopping_patience=20, plot_training_progress=False, verbose=1)
+#
+
+# history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
+#           batch_size=32, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
+#
+#
+#
+# history1 = nnm.train(inputs=train_ts, targets=train_sum,validation_inputs=validation_ts,validation_targets=validation_sum,
+#           batch_size=4096, epochs=40*10, val_freq=1, early_stopping_patience=5, plot_training_progress=False, verbose=1)
+
+
+# train_pred = nnm.predict(train_ts)
+# train_mae = np.mean(abs(train_pred-train_sum),axis=0)
+# print("train_mae: ", train_mae)
+#
+# test_pred = nnm.predict(test_ts)
+# test_mae = np.mean(abs(test_pred-test_sum),axis=0)
+# print("test_mae: ", test_mae)
+#
+# print("test sum std: ", np.std(test_sum,0))
+# print("test sum mean: ", np.mean(test_sum,0))
+# print("optimal guessing deviation: ", np.mean(abs(test_sum - np.mean(test_sum,0)),0))
+#
+# obs_pred = nnm.predict(obs_data)
+#
+# print("observed data:")
+# for pred, true in zip(obs_pred,obs_sum):
+#     print("pred: ", pred, ", true: ", true)
+#
+# print()
+# print("test samples:")
+#
+# for i in range(25):
+#     print("test_sum: ", test_sum[i], ", test_pred: ", test_pred[i])
