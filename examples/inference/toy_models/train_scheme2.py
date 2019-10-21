af4c08cc2228dd3cf9f045b96adc998948183519
Mattias <ma10s@hotmail.com>
2019-10-22 00:04:05 +0200

    k

--- examples/inference/toy_models/train_scheme2.py
+++ examples/inference/toy_models/train_scheme2.py
@@ -3,4 +3,4 @@ from train_vilar_all_species_func import train_routine
 species = [[8],[6,8]]
 datanames = ["R","C_and_R"]
 for i in range(2):
-    train_routine(species=species[i],dataname=datanames)
+    train_routine(species=species[i],dataname=datanames[i])
