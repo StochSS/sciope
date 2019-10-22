929083d7e8e0bafeec552d5f8a2996b16e675eeb
Mattias <ma10s@hotmail.com>
2019-10-22 18:54:00 +0200

    k

--- examples/inference/toy_models/my_ss_search.py
+++ examples/inference/toy_models/my_ss_search.py
@@ -55,7 +55,8 @@ true_param = [50.0, 500.0, 0.01, 50.0, 50.0, 5.0, 10.0, 0.5, 1.0, 0.2, 1.0, 1.0,
 nr_of_accept = 1000
 dist = np.linalg.norm(train_sum - observed_sum, axis=1)
 print("dist shape: ", dist.shape)
-plt.hist(dist)
+bins=np.linspace(0,4000,400)
+plt.hist(dist,bins=bins)
 plt.savefig("dist_plot")
 plt.close()
 
