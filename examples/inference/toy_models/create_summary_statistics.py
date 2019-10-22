b15a58ce29e350603a5e2ad3e395d0a058c361bd
Mattias <ma10s@hotmail.com>
2019-10-22 16:53:26 +0200

    k

--- examples/inference/toy_models/create_summary_statistics.py
+++ examples/inference/toy_models/create_summary_statistics.py
@@ -1,11 +1,5 @@
-import matplotlib.pyplot as plt
-
-import pickle
 import numpy as np
-import time
-import dask
 
-species=6
 
 def find_peaks(ts):
     peaks = []
@@ -13,12 +7,10 @@ def find_peaks(ts):
         left = np.maximum(0,t-10)
         right = np.minimum(ts.shape[0],t+10)
         ind =np.argmax(ts[left:right]) + left
-
-        # print("t", t, ", ind: ", ind, ", left: ", left, ", right: ", right)
-
         if ts[ind]>np.max(ts)*0.5 and ind==t:
             peaks.append(ind)
     return peaks
+
 
 def peak_dist(peaks):
     dist = []
@@ -26,11 +18,13 @@ def peak_dist(peaks):
         dist.append(peaks[i+1]-peaks[i])
     return dist
 
+
 def mean_dist(dist):
     if dist:
         return np.mean(dist)
     else:
         return -1
+
 
 def std_dist(dist):
     if dist:
@@ -39,34 +33,30 @@ def std_dist(dist):
         return -1
 
 
+def mean_peak(ts,peaks):
+    if peaks:
+        return np.mean(ts(peaks))
+    else:
+        return -1
 
-def summarys(ts,itt=-1):
-    if itt%1000==0:
-        print(itt)
+def std_peak(ts,peaks):
+    if peaks:
+        return np.std(ts(peaks))
+    else:
+        return -1
+
+
+def summarys(ts,ind=None):
+    if ind:
+        if ind%1000==0:
+            print("ind: ", ind)
     peaks = find_peaks(ts)
     dist = peak_dist(peaks)
     m_dist = mean_dist(dist)
     s_dist = std_dist(dist)
+    m_peaks = mean_peak(ts, peaks)
+    s_peaks = std_peak(ts, peaks)
 
-    return np.array([m_dist,s_dist])
+    return np.array([m_dist,s_dist, m_peaks, s_peaks])
 
 
-# validation_ts = pickle.load(open('validation_ts.p', "rb" ) )[:,:,species]
-#
-# print("data loaded")
-# start = time.time()
-# val_sum = np.array([summarys(ts) for ts in validation_ts])
-# end = time.time()
-# print("summarys generated in ", end-start)
-# print(val_sum[200:210])
-
-# f,ax = plt.subplots(4)
-# j=0
-# for i in range(16,20):
-#     max_value = np.max(validation_ts[i])
-#     peaks = find_peaks(validation_ts[i])
-#     ax[j].plot(validation_ts[i])
-#     for p in peaks:
-#         ax[j].plot([p,p], [max_value,0])
-#     j+=1
-# plt.show()
---
+++ examples/inference/toy_models/my_ss_search.py
@@ -0,0 +1,38 @@
+from create_summary_statistics import summarys
+
+
+import numpy as np
+
+import pickle
+import time
+from normalize_data import normalize_data, denormalize_data
+from load_data import load_spec
+import vilar
+from vilar import Vilar_model
+
+
+
+
+
+num_timestamps=401
+endtime=200
+modelname = "vilar_ACR_" + str(endtime) + "_" + str(num_timestamps) + '_all_species'
+# parameter range
+dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
+dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
+
+#6=C, 7=A, 8=R
+species = [6]
+
+
+
+
+#Load data
+train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
+print("load train data done!")
+
+train_sum = np.array([summarys(train_ts[i],i) for i in range(len(train_ts))])
+
+pickle.dump(train_sum, open('datasets/' + modelname + '/train_sum.p', "wb"))
+
+print("done!")
