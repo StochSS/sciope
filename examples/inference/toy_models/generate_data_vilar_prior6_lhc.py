5ef84518387637d560dec700da25a5253bf75498
Mattias <ma10s@hotmail.com>
2019-10-24 12:44:45 +0200

    k

--- examples/inference/toy_models/generate_data_vilar_prior6_lhc.py
+++ examples/inference/toy_models/generate_data_vilar_prior6_lhc.py
@@ -95,10 +95,18 @@ if not os.path.exists('datasets/lhc2'):
 if not os.path.exists('datasets/lhc2/' + modelname):
     os.mkdir('datasets/lhc2/' + modelname)
 
+if not os.path.isfile('datasets/lhc/' + modelname + '/train_thetas_'+str(1)+'.p'):
+    lhs_obj = lhs.LatinHypercube(dmin, dmax)
+    lhs_delayed = lhs_obj.generate(training_samples)
+    print("delayed")
+    print("lhs_delayed shape: ", lhs_delayed.shape)
+    train_thetas, = dask.compute(lhs_delayed)
 
-train_thetas = lhc_sampling(n=50000, dim=15, dmin=dmin, dmax=dmax).T
-print("train_thetas shape: ", train_thetas.shape)
-pickle.dump(train_thetas, open('datasets/lhc2/' + modelname + '/train_thetas_.p', "wb"))
+
+    print("train_thetas shape: ", train_thetas.shape)
+    train_thetas = np.squeeze(train_thetas)
+    print("train_thetas shape: ", train_thetas.shape)
+    pickle.dump(train_thetas, open('datasets/lhc2/' + modelname + '/train_thetas_.p', "wb"))
 
 bs = 1000
 
