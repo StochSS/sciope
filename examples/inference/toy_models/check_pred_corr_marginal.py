ea0fbf6811e8ccc1c4336b4cc12a061f6d8fc69c
Mattias <ma10s@hotmail.com>
2019-10-26 11:16:15 +0200

    k

--- examples/inference/toy_models/check_pred_corr_marginal.py
+++ examples/inference/toy_models/check_pred_corr_marginal.py
@@ -120,21 +120,21 @@ for i in range(nrs):
     nr_of_accept2 = 10
     print("accepted para shape: ", accepted_para.shape)
 
-    linew = 2
-    f, ax = plt.subplots(3,5,figsize=(20,20))
+    linew = 3
+    f, ax = plt.subplots(3,5,figsize=(40,20))
 
     for x in range(3):
         for y in range(5):
             i = x*5+y
             # points = int((1/test_ae_norm[i]))
-            points = int((1/np.std(normalize_data(accepted_para,dmin,dmax)[:,i]))**2)
+            points = int((1/np.std(normalize_data(accepted_para,dmin,dmax)[:,i]))**1.5)
             print("i: ", i, ", points: ", points)
             bins = np.linspace(dmin[i],dmax[i],points)
             ret = ax[x,y].hist(accepted_para[:,i], bins=bins, color='g',alpha=0.5)
             peakv = np.max(ret[0])
-            ax[x,y].plot([true_params[0][i], true_params[0][i]], [peakv, 0],lw=linew, c='black')
             ax[x,y].plot([dmin[i], dmin[i]], [peakv, 0], c='b', lw=linew)
             ax[x,y].plot([dmax[i], dmax[i]], [peakv, 0], c='b', lw=linew)
+            ax[x,y].plot([true_params[0][i], true_params[0][i]], [peakv, 0],lw=linew, ls='--', c='black')
 
 
 
