698f2963c66473a602bd1ca5fa83e3c20c603da2
Mattias <ma10s@hotmail.com>
2019-10-28 16:20:17 +0100

    k

--- examples/inference/toy_models/check_pred_corr_linear_regr.py
+++ examples/inference/toy_models/check_pred_corr_linear_regr.py
@@ -120,12 +120,12 @@ for j in range(15):
 accepted_para_hist = []
 print("nrs: ", nrs)
 bin_box_tot = []
-for i in range(30,30+nrs):
+for i in range(0,0+nrs):
     print("i: ", i)
 
     od = obs_data_big[[i]]
     # print("od shape: ", od.shape)
-    accepted_para, accepted_pred, data_pred = abc_inference_linear_regression(data=od, abc_trial_thetas=test_thetas,abc_trial_ts=test_ts, nnm=nnm, nr_of_accept = 1000)
+    accepted_para, accepted_pred, data_pred = abc_inference_linear_regression(data=od, abc_trial_thetas=test_thetas,abc_trial_ts=test_ts, nnm=nnm, nr_of_accept = 100)
     # print("data_pred shape: ", data_pred.shape)
     accepted_para_hist.append(accepted_para)
     data_pred = denormalize_data(data_pred,dmin,dmax)
