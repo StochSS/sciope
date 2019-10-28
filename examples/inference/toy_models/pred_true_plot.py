769fa7826e1b32161b6ab614f0dd9ee1f4552d33
Mattias <ma10s@hotmail.com>
2019-10-28 10:46:14 +0100

    k

--- examples/inference/toy_models/pred_true_plot.py
+++ examples/inference/toy_models/pred_true_plot.py
@@ -12,7 +12,7 @@ def heatmap(true_thetas,pred_thetas, dmin, dmax, true_point=None, pred_point=Non
         for y in range(5):
             j = x * 5 + y
             ax[x,y].plot([dmin[j], dmin[j], dmax[j], dmax[j], dmin[j]],[dmin[j], dmax[j], dmax[j], dmin[j], dmin[j]],c='w',lw=4)
-            ax[x,y].hist2d(true_thetas[:,j],pred_thetas[:,j],bins=100,cmap='terrain')
+            ax[x,y].hist2d(true_thetas[:,j],pred_thetas[:,j],bins=100,cmap='terrain', range=[[dmin[j], dmax[j]],[dmin[j], dmax[j]]])
             ax[x,y].plot([dmin[j], dmin[j], dmax[j], dmax[j], dmin[j]],[dmin[j], dmax[j], dmax[j], dmin[j], dmin[j]],c='w',lw=4)
             ax[x,y].plot([dmin[j],dmax[j]],[dmin[j],dmax[j]], lw=5, c='w', ls=':')
 
