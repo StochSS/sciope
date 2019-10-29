015c779b824905b30a3aa49f32fac7f415b45b07
Mattias <ma10s@hotmail.com>
2019-10-29 11:21:07 +0100

    k

---
+++ examples/inference/toy_models/train_scheme_compare_architectures.py
@@ -0,0 +1,16 @@
+from train_func import train_routine
+
+
+num_timestamps=401
+endtime=200
+modelname = "vilar_ACR_prior6" + str(endtime) + "_" + str(num_timestamps)
+# parameter range
+dmin = [30, 200, 0, 30, 30, 1, 1, 0, 0, 0, 0.5, 0.5, 1, 30, 80]
+dmax = [70, 600, 1, 70, 70, 10, 12, 1, 2, 0.5, 1.5, 1.5, 3, 70, 120]
+species = [0]
+dataname = "C"
+models = ['CNN', 'PEN', 'DNN']
+for i in range(3):
+    train_routine(modelname=modelname, dmin=dmin, dmax=dmax, species=species,dataname=dataname, model=models[i])
+
+
