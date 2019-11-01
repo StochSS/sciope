b2a165bde6f9d170adb4022a2d18d345b14cccb4
Mattias <ma10s@hotmail.com>
2019-11-01 23:48:04 +0100

    k

--- examples/inference/toy_models/train_scheme_all_subset_prior6.py
+++ examples/inference/toy_models/train_scheme_all_subset_prior6.py
@@ -7,7 +7,8 @@ modelname = "vilar_ACR_prior6_" + str(endtime) + "_" + str(num_timestamps)
 # parameter range
 dmin = [0,    100,    0,   20,   10,   1,    1,   0,   0,   0, 0.5,    0,   0,    0,   0]
 dmax = [80,   600,    4,   60,   60,   7,   12,   2,   3, 0.7, 2.5,   4,   3,   70,   300]
-species = np.arange(9)
+species = [[i] for i in range(9)]
+print("species len: ", len(species))
 dataname = "new_prior6"
 models = ['CNN'] #, 'PEN', 'DNN']
 # models = ['PEN', 'DNN']
