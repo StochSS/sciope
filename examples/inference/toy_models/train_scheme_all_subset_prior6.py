3e0d90c2b2371d34de22e07aad46f7b60bbd915a
Mattias <ma10s@hotmail.com>
2019-11-01 23:41:03 +0100

    k

--- examples/inference/toy_models/train_scheme_all_subset_prior6.py
+++ examples/inference/toy_models/train_scheme_all_subset_prior6.py
@@ -1,5 +1,5 @@
 from train_function import train_routine
-
+import numpy as np
 
 num_timestamps=401
 endtime=200
@@ -8,12 +8,12 @@ modelname = "vilar_ACR_prior6_" + str(endtime) + "_" + str(num_timestamps)
 dmin = [0,    100,    0,   20,   10,   1,    1,   0,   0,   0, 0.5,    0,   0,    0,   0]
 dmax = [80,   600,    4,   60,   60,   7,   12,   2,   3, 0.7, 2.5,   4,   3,   70,   300]
 species = np.arange(9)
-dataname = "C"
-models = ['CNN', 'PEN', 'DNN']
+dataname = "new_prior6"
+models = ['CNN'] #, 'PEN', 'DNN']
 # models = ['PEN', 'DNN']
 
-for i in range(3):
-    train_routine(modelname=modelname, dmin=dmin, dmax=dmax, species=species,dataname=dataname,
-                  pooling_len=2, dlay=[400,400,400], model=models[i])
+for i in range(9):
+    train_routine(modelname=modelname, dmin=dmin, dmax=dmax, species=species[9],dataname=dataname,
+                  pooling_len=2, dlay=[400,400,400], model='CNN')
 
 
