d1ee5acaf90cf13477d567e1f75f3c80187b1110
Mattias <ma10s@hotmail.com>
2019-11-05 22:02:59 +0100

    k

--- examples/inference/toy_models/train_scheme_compare_architectures.py
+++ examples/inference/toy_models/train_scheme_compare_architectures.py
@@ -9,8 +9,8 @@ dmin = [0,    100,    0,   20,   10,   1,    1,   0,   0,   0, 0.5,    0,   0,  
 dmax = [80,   600,    4,   60,   60,   7,   12,   2,   3, 0.7, 2.5,   4,   3,   70,   300]
 species = [6]
 dataname = "C"
-models = ['CNN', 'PEN', 'DNN']
-# models = ['PEN', 'DNN']
+# models = ['CNN', 'PEN', 'DNN']
+models = ['PEN', 'DNN']
 
 for i in range(3):
     train_routine(modelname=modelname, dmin=dmin, dmax=dmax, species=species,dataname=dataname,
