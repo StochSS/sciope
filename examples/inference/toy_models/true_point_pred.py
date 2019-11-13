7ed34ee3d2adb5cbc341887b8a9665632a258828
Mattias <ma10s@hotmail.com>
2019-11-13 20:50:55 +0100

    k

---
+++ examples/inference/toy_models/true_point_pred.py
@@ -0,0 +1,79 @@
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from sciope.inference import abc_inference
+from sciope.models.cnn_regressor import CNNModel
+from sciope.models.pen_regressor_beta import PEN_CNNModel
+from sciope.models.dnn_regressor import ANNModel
+from abc_rejection_sampling import abc_inference
+
+import numpy as np
+
+import pickle
+import time
+from normalize_data import normalize_data, denormalize_data
+from load_data import load_spec
+from vilar_all_species import Vilar_model
+import vilar
+
+
+
+num_timestamps=401
+endtime=200
+
+modelname = "vilar_ACR_prior6_" + str(endtime) + "_" + str(num_timestamps)
+
+dmin = [0,    100,    0,   20,   10,   1,    1,   0,   0,   0, 0.5,    0,   0,    0,   0]
+dmax = [80,   600,    4,   60,   60,   7,   12,   2,   3, 0.7, 2.5,   4,   3,   70,   300]
+
+
+#5,6,7,8
+#6=C, 7=A, 8=R
+species = [0]
+
+clay=[32,48,64,96]
+ts_len = 401
+nr_of_species = len(species)
+
+# choose neural network model
+nnm = CNNModel(input_shape=(ts_len,nr_of_species), output_shape=(15), con_len=3, con_layers=clay, dense_layers=[400,400,400],dataname='speciesC')
+
+print("Model name: ", nnm.name)
+
+
+true_param = pickle.load(open('datasets/' + modelname + '/true_param.p', "rb" ) )
+true_param = np.squeeze(np.array(true_param))
+print("true_param shape: ", true_param.shape)
+data = pickle.load(open('datasets/' + modelname + '/obs_data_pack.p', "rb" ) )
+# data = data[3]
+# data = np.expand_dims(data,0)
+print("data shape: ", data.shape)
+
+nnm.load_model()
+
+
+test_thetas = pickle.load(open('datasets/' + modelname + '/test_thetas.p', "rb" ) )
+test_ts = pickle.load(open('datasets/' + modelname + '/test_ts.p', "rb" ) )
+test_ts = test_ts[:,:,species]
+test_thetas_n = normalize_data(test_thetas,dmin,dmax)
+test_pred = nnm.predict(test_ts)
+test_pred = np.reshape(test_pred,(-1,15))
+test_pred_d = denormalize_data(test_pred,dmin,dmax)
+test_mse = np.mean((test_thetas-test_pred)**2)
+test_mae = np.mean(abs(test_thetas-test_pred_d))
+test_ae = np.mean(abs(test_thetas-test_pred_d),axis=0)
+test_ae_norm = np.mean(abs(test_thetas_n-test_pred),axis=0)
+
+train_thetas, train_ts = load_spec(modelname=modelname, type = "train", species=species)
+print("train_ts shape: ", train_ts.shape)
+end_step=401
+step = 1
+train_ts = train_ts[:,:end_step:step,species]
+
+
+
+print("Model name: ", nnm.name)
+print("mean square error: ", test_mse)
+print("mean rel absolute error: ", np.mean(test_ae_norm))
