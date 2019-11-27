0e1a3ab5ca2b59fe343c9ae6ef4694f91bff0dd3
Fredrik Wrede <wrede.fredrik@gmail.com>
2019-11-27 18:02:32 +0100

    wrapper for gillespy2

---
+++ sciope/utilities/gillespy2/wrapper.py
@@ -0,0 +1,45 @@
+# Copyright 2017 Prashant Singh, Fredrik Wrede and Andreas Hellander
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Simulator wrapper for gillespy2 models
+
+"""
+
+# Imports
+import numpy as np
+
+
+def _set_model_parameters(params, model):
+    """ params - array, needs to have the same order as model.listOfParameters """
+    for e, (pname, p) in enumerate(model.listOfParameters.items()):
+        model.get_parameter(pname).set_expression(params[e])
+    return model
+
+def _simulator(params, model, kwargs, species_of_interest):
+    
+    model_update = _set_model_parameters(params, model)
+
+    res = model_update.run(**kwargs)
+    
+    tot_res = []
+    if type(res[0]) == np.ndarray:
+        tot_res = np.asarray([x.T for x in res]) # reshape to (N, S, T)  
+        tot_res = tot_res[:,1:, :] # should not contain timepoints
+    else:
+        tot_res = np.asarray([np.asarray(list(x.values())[1:]) for x in res])
+    return tot_res
+
+def get_simulator(gillespy_model,  run_settings, species_of_interest=None):
+    
+    return lambda x : _simulator(x, model=gillespy_model, kwargs=run_settings)
