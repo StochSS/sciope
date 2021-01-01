6a59faf92adbbc02f146326137a8f129cdbbe9a5
Prashant Singh <contact@prashantsingh.se>
2021-01-01 23:43:05 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2',
+    version='0.3',
 
     description='The sciope toolbox for surrogate modeling, likelihood-free parameter inference and black-box optimization.',
     long_description_content_type='text/markdown',
